28b192a11c62b036b3752b0c99a6d4cbe74fb56b
Rooitier <rooitier@gmail.com>
2023-10-06 16:18:49 +0200

    slow day, struggling to understand TAMEX

--- c4data/bplastData/bPlastTamexData.cxx
+++ c4data/bplastData/bPlastTamexData.cxx
@@ -3,14 +3,22 @@
 bPlastTamexData::bPlastTamexData()
     : fDet(0)
     , fChan(0)
-    , fPMT_Lead(0)
+    , fbPlastLeadT(0)
+    , fbPlastTrailT(0)
+    , fbPlastToT(0)
 {
 }
 
-bPlastTamexData::bPlastTamexData(UInt_t detNum, UInt_t chanNum, UInt_t PMT_Lead)
+bPlastTamexData::bPlastTamexData(UInt_t detNum, 
+                                    UInt_t chanNum, 
+                                    UInt_t fbPlastLeadT, 
+                                    UInt_t fbPlastTrailT, 
+                                    UInt_t fbPlastToT)
     : fDet(detNum)
     , fChan(chanNum)
-    , fPMT_Lead(PMT_Lead)
+    , fbPlastLeadT(fbPlastLeadT)
+    , fbPlastTrailT(fbPlastTrailT)
+    , fbPlastToT(fbPlastToT)
 {
 }
 
