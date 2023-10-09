37233c534ab74d96512621374a5c07f25c96e8e8
Nicolas Hubbard <n.hubbard@gsi.de>
2023-10-09 18:24:15 +0200

    Add AidaCalData class

--- c4data/aidaData/AidaCalData.cxx
+++ c4data/aidaData/AidaCalData.cxx
@@ -1,1 +1,27 @@
-#include stuff
+#include "AidaCalData.h"
+
+AidaCalAdcItem::AidaCalAdcItem()
+{
+}
+
+void AidaCalAdcItem::Reset()
+{
+    slowTime = fastTime = 0;
+    dssd = side = strip = 0;
+    range = false;
+    intensity = energy = 0.;
+}
+
+void AidaCalAdcItem::SetAll(uint64_t st, uint64_t ft, int d, int si, int str, bool r, double i, double e)
+{
+    slowTime = st;
+    fastTime = ft;
+    dssd = d;
+    side = si;
+    strip = str;
+    range = r;
+    intensity = i;
+    energy = e;
+}
+
+ClassImp(AidaCalAdcItem)
--- c4data/aidaData/AidaCalData.h
+++ c4data/aidaData/AidaCalData.h
@@ -1,1 +1,76 @@
-#include "stuff"
+#ifndef AidaCalData_H
+#define AidaCalData_H
+
+#include "TObject.h"
+
+class AidaCalAdcItem : public TObject
+{
+public:
+    AidaCalAdcItem();
+
+    void Reset();
+    void SetAll(uint64_t st, uint64_t ft, int d, int si, int str, bool r, double i, double e);
+    uint64_t SlowTime() const;
+    uint64_t FastTime() const;
+    int DSSD() const;
+    int Side() const;
+    int Strip() const;
+    bool Range() const;
+    double Intensity() const;
+    double Energy() const;
+
+    ClassDefNV(AidaCalAdcItem, 2)
+
+private:
+    uint64_t slowTime;
+    uint64_t fastTime;
+    int dssd;
+    int side;
+    int strip;
+    bool range;
+    double intensity; // only for gain matching code?
+    double energy;
+};
+
+inline uint64_t AidaCalAdcItem::SlowTime() const
+{
+    return slowTime;
+}
+
+inline uint64_t AidaCalAdcItem::FastTime() const
+{
+    return fastTime;
+}
+
+inline int AidaCalAdcItem::DSSD() const
+{
+    return dssd;
+}
+
+inline int AidaCalAdcItem::Side() const
+{
+    return side;
+}
+
+inline int AidaCalAdcItem::Strip() const
+{
+    return strip;
+}
+
+inline bool AidaCalAdcItem::Range() const
+{
+    return range;
+}
+
+inline double AidaCalAdcItem::Intensity() const
+{
+    return intensity;
+}
+
+inline double AidaCalAdcItem::Energy() const
+{
+    return energy;
+}
+
+#endif /* AidaCalData_H */
+
