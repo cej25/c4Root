1e416dde8a2f2142a0ddd85e2e76b593992fc47b
Nicolas Hubbard <n.hubbard@gsi.de>
2023-10-12 13:31:39 +0200

    Move aida algorithms to new aida folder

--- macros/despec/run_aida_online.C
+++ macros/despec/run_aida_online.C
@@ -1,7 +1,7 @@
 typedef struct EXT_STR_h101_t
 {   
     EXT_STR_h101_aida_onion_t aida;
-    EXT_STR_h101_BPLAST_onion_t bplast;
+    EXT_STR_h101_GE_FEBEX_onion_t germanium; // why can't it find this :()
 } EXT_STR_h101;
 
 void run_aida_online(const Int_t nev = -1, const Int_t fRunId = 1, const Int_t fExpId = 1)
@@ -22,15 +22,16 @@ void run_aida_online(const Int_t nev = -1, const Int_t fRunId = 1, const Int_t f
     FairLogger::GetLogger()->SetColoredLog(true);
 
     TString filename = "~/lustre/gamma/DESPEC_S452_FILES/newts/S452f103_0037.lmd";
+    //TString filename = "trans://lxg1257";
     TString outputpath = "~/run_online_ge_test";
     TString outputFileName = outputpath + ".root";
 
     Int_t refresh = 10; // Refresh rate for online histograms
     Int_t port = 5999; // Port number for online visualisation, e.g. lxgXXXX:8886
      
-    TString ntuple_options = "UNPACK:aida,RAW,NOTRIGEVENTNO"; // "RAW"? "time=stitch=1000"? can we time-stitch files here pls?
-    TString ucesb_dir = getenv("UCESB_DIR"); // .bashrc
-    TString ucesb_path = ucesb_dir + "/s452/s452 --allow-errors --input-buffer=200Mi"; // CEJ: R3B used input-buffer, can't see in ucesb doc however...
+    TString ntuple_options = "UNPACK:aida,RAW"; // "RAW"? "time=stitch=1000"? can we time-stitch files here pls?
+    std::string ucesb_dir = getenv("UNPACKER"); // .bashrc
+    TString ucesb_path = ucesb_dir + "/s452 --allow-errors --input-buffer=200Mi"; // CEJ: R3B used input-buffer, can't see in ucesb doc however...
     ucesb_path.ReplaceAll("//","/");
 
     // Create online run
@@ -47,30 +48,35 @@ void run_aida_online(const Int_t nev = -1, const Int_t fRunId = 1, const Int_t f
     // Create source using ucesb for input
     UcesbSource* source = new UcesbSource(filename, ntuple_options, ucesb_path, &ucesb_struct, sizeof(ucesb_struct));
     source->SetMaxEvents(nev);
+
+    TAidaConfiguration::SetBasePath(ucesb_dir + "/config/AIDA");
    
     //GermaniumReader* unpackgermanium = new GermaniumReader((EXT_STR_h101_GE_FEBEX_onion*)&ucesb_struct.germanium, offsetof(EXT_STR_h101, germanium));
     AidaReader* unpackaida = new AidaReader((EXT_STR_h101_aida_onion*)&ucesb_struct.aida, offsetof(EXT_STR_h101, aida));
-    bPlastReader* unpackbplast = new bPlastReader((EXT_STR_h101_BPLAST_onion*)&ucesb_struct.bplast, offsetof(EXT_STR_h101, bplast));
 
     // Add readers
     //unpackgermanium->SetOnline(false);
     //source->AddReader(unpackgermanium);
 
-    
-    unpackaida->SetOnline(false);
+    unpackaida->SetOnline(true);
     source->AddReader(unpackaida);
-    unpackbplast->SetOnline(false);
-    source->AddReader(unpackbplast);
 
     run->SetSource(source);
 
     // Runtime data base
     FairRuntimeDb* rtdb = run->GetRuntimeDb();
 
+    // Aida processing tasks
+    AidaUnpack2Cal* aidaCalibrator = new AidaUnpack2Cal();
+    aidaCalibrator->SetOnline(true);
+    run->AddTask(aidaCalibrator);
+
 
     // Add analysis task
     //GermaniumOnlineSpectra* online = new GermaniumOnlineSpectra();
     //run->AddTask(online);
+    AidaOnlineSpectra* aidaOnline = new AidaOnlineSpectra();
+    run->AddTask(aidaOnline);
 
     // Initialise
     run->Init();
