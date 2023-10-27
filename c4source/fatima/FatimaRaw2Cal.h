6d8853740e4979b79986cfc18c6c5e80792493a4
Johan Emil Linnestad Larsson <jlarsson@lxi090.gsi.de>
2023-10-27 15:40:24 +0200

    Added timemachine stuff + bug fixes

--- c4source/fatima/FatimaRaw2Cal.h
+++ c4source/fatima/FatimaRaw2Cal.h
@@ -8,6 +8,7 @@ class TClonesArray;
 class EventHeader;
 class FatimaTwinpeaksData;
 class FatimaTwinpeaksCalData;
+class TimeMachineData;
 
 class FatimaRaw2Cal : public FairTask
 {
@@ -24,6 +25,7 @@ class FatimaRaw2Cal : public FairTask
         Bool_t SetDetectorMapFile(TString);
         Bool_t SetDetectorCalFile(TString);
 
+        void SetTimeMachineChannels(int ftime_machine_delayed_detector_id, int ftime_machine_undelayed_detector_id);
 
         void Exec(Option_t* option);
 
@@ -42,11 +44,13 @@ class FatimaRaw2Cal : public FairTask
 
         TClonesArray* fcal_data;
         TClonesArray* funcal_data;
+        TClonesArray* ftime_machine_array;
 
 
         FatimaTwinpeaksData* funcal_hit;
         FatimaTwinpeaksData* funcal_hit_next; // this is the slow or fast hit corresponding to the fast or slow hit :)
         FatimaTwinpeaksCalData* fcal_hit;
+        //TimeMachineData* ftime_machine_hit;
 
         uint16_t detector_id;
         
@@ -67,6 +71,10 @@ class FatimaRaw2Cal : public FairTask
         //internal status flags for detector map and calibration map:
         Bool_t DetectorMap_loaded = 0;
         Bool_t DetectorCal_loaded = 0;
+
+        int time_machine_delayed_detector_id;
+        int time_machine_undelayed_detector_id;
+
         //maps:
         std::map<std::pair<int,int>,int> detector_mapping; // [board_id][channel_id] -> [detector_id]
         std::map<int,std::pair<double,double>> calibration_coeffs; // key: [detector id] -> [a0][a1]
