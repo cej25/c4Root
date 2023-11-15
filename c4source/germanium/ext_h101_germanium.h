d9fc53b195179bb1f06a5c9c15789b693410e5a0
cej25 <c.jones@gsi.de>
2023-11-15 15:19:13 +0100

    struct header files

--- c4source/germanium/ext_h101_germanium.h
+++ c4source/germanium/ext_h101_germanium.h
@@ -26,7 +26,7 @@ typedef          int  int32_t;
  * Plain structure (layout as ntuple/root file):
  */
 
-typedef struct EXT_STR_h101_t
+typedef struct EXT_STR_h101_GERMANIUM_t
 {
   /* UNPACK */
   uint32_t germanium_data1event_trigger_time_hi /* [-1,-1] */;
@@ -239,7 +239,7 @@ typedef struct EXT_STR_h101_t
   uint32_t germanium_ts_t3 /* [0,65535] */;
   uint32_t germanium_ts_t4 /* [0,65535] */;
 
-} EXT_STR_h101;
+} EXT_STR_h101_GERMANIUM;
 
 /********************************************************
  *
@@ -247,7 +247,7 @@ typedef struct EXT_STR_h101_t
  * recovered (recommended):
  */
 
-typedef struct EXT_STR_h101_onion_t
+typedef struct EXT_STR_h101_GERMANIUM_onion_t
 {
   /* UNPACK */
   struct {
@@ -276,11 +276,11 @@ typedef struct EXT_STR_h101_onion_t
   uint32_t germanium_ts_subsystem_id;
   uint32_t germanium_ts_t[4];
 
-} EXT_STR_h101_onion;
+} EXT_STR_h101_GERMANIUM_onion;
 
 /*******************************************************/
 
-#define EXT_STR_h101_ITEMS_INFO(ok,si,offset,struct_t,printerr) do { \
+#define EXT_STR_h101_GERMANIUM_ITEMS_INFO(ok,si,offset,struct_t,printerr) do { \
   ok = 1; \
   /* UNPACK */ \
   EXT_STR_ITEM_INFO2    (ok,si,offset,struct_t,printerr,\
