ba0a64e9f5a43ae4a7db030e985ceafa2e99bf8b
mrudigier <m.rudigier@gmx.de>
2023-11-16 14:29:25 +0100

    fixed Makefile for NovTest (only, do not commit to Main)

--- unpack/exps/NovTest/obj_NovTest/markconvbold.d
+++
@@ -1,4 +0,0 @@
-markconvbold.o obj_NovTest/markconvbold.d : /u/jlarsson/ucesb/lu_common/markconvbold.cc \
- /u/jlarsson/ucesb/lu_common/markconvbold.c \
- /u/jlarsson/ucesb/lu_common/markconvbold.h \
- /u/jlarsson/ucesb/lu_common/colourtext.h
