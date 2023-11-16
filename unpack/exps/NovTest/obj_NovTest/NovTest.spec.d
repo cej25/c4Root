7ec5f00995bd416fa06db40faeeccb5bdc98e3c3
mrudigier <m.rudigier@gmx.de>
2023-11-16 14:29:25 +0100

    fixed Makefile for NovTest (only, do not commit to Main)

--- unpack/exps/NovTest/obj_NovTest/NovTest.spec.d
+++
@@ -1,3 +0,0 @@
-gen_NovTest/NovTest.uce NovTest.spec.d : NovTest.spec ../../common/whiterabbit.spec \
- ../../common/gsi_tamex4.spec ../../common/gsi_febex4.spec \
- ../../../config/NovTest/setup.hh
--- unpack/exps/NovTest/obj_NovTest/colourtext.d
+++
@@ -1,3 +0,0 @@
-colourtext.o obj_NovTest/colourtext.d : /u/jlarsson/ucesb/lu_common/colourtext.cc \
- /u/jlarsson/ucesb/lu_common/colourtext.c \
- /u/jlarsson/ucesb/lu_common/colourtext.h
