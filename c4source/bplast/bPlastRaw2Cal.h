83d2fd77e106ddf281a3f6cf00a5c60f38e3af67
Johan Emil Linnestad Larsson <jlarsson@lxi090.gsi.de>
2023-11-21 16:39:47 +0100

    TimeMachine into bplast

--- c4source/bplast/bPlastRaw2Cal.h
+++ c4source/bplast/bPlastRaw2Cal.h
@@ -22,6 +22,8 @@ class bPlastRaw2Cal : public FairTask
 
         void PrintDetectorMap();
         void PrintDetectorCal();
+        
+        void SetTimeMachineChannels(int ftime_machine_delayed_detector_id, int ftime_machine_undelayed_detector_id);
 
         Bool_t SetDetectorMapFile(TString);
         Bool_t SetDetectorCalFile(TString);
@@ -44,6 +46,7 @@ class bPlastRaw2Cal : public FairTask
 
         TClonesArray* fcal_data;
         TClonesArray* funcal_data;
+        TClonesArray* ftime_machine_array;
 
 
         bPlastTwinpeaksData* funcal_hit;
@@ -65,6 +68,10 @@ class bPlastRaw2Cal : public FairTask
 
         EventHeader * header;
         Int_t fNEvents = 0;
+
+        
+        int time_machine_delayed_detector_id;
+        int time_machine_undelayed_detector_id;
 
         //internal status flags for detector map and calibration map:
         Bool_t DetectorMap_loaded = 0;
