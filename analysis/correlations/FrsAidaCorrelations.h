2d4b744cb7063ab1167f635514ae2e78320636b7
cej25 <c.jones@gsi.de>
2024-02-11 19:10:05 +0100

    beginning to set up Correlations class, adding LICENSE

--- analysis/correlations/FrsAidaCorrelations.h
+++ analysis/correlations/FrsAidaCorrelations.h
@@ -1,6 +1,16 @@
 #ifndef FrsAidaCorrelations_H
 #define FrsAidaCorrelations_H
 
+class FrsAidaCorrelations : public FairTask
+{
+    public:
+        FrsAidaCorrelations();
+
+        virtual ~FrsAidaCorrelations();
+
+    private:
+        
+}
 
 
 
