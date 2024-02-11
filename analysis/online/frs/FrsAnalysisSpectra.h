0833394c734b871d7657b942733f9bfb1d0828de
cej25 <c.jones@gsi.de>
2024-02-11 21:55:50 +0100

    making progress with FRS anal/correl(aida) spectra

--- analysis/online/frs/FrsAnalysisSpectra.h
+++ analysis/online/frs/FrsAnalysisSpectra.h
@@ -1,12 +1,14 @@
 #ifndef FrsAnalysisSpectra_H
 #define FrsAnalysisSpectra_H
 
+#include "FairTask.h"
+
 class FrsAnalysisSpectra : public FairTask
 {
     public:
         FrsAnalysisSpectra();
 
         virtual ~FrsAnalysisSpectra();
-}
+};
 
 #endif
