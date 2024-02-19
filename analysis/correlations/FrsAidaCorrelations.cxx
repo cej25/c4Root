bea18dbdb436386e9fd09903f97c5f42341b65d2
cej25 <c.jones@gsi.de>
2024-02-19 14:21:41 +0100

    FRS TAC analysis histos seem to work/fill. Default gates are awful and hence nothing real can be checked. Ta-da!

--- analysis/correlations/FrsAidaCorrelations.cxx
+++ analysis/correlations/FrsAidaCorrelations.cxx
@@ -11,7 +11,7 @@
 
 #include <vector>
 
-FrsAidaCorrelations::FrsAidaCorrelations()
+FrsAidaCorrelations::FrsAidaCorrelations(std::vector<std::vector<TCutG*>> fFrsGates)
     :   FairTask()
     ,   fNEvents()
     ,   header(nullptr)
@@ -21,6 +21,40 @@ FrsAidaCorrelations::FrsAidaCorrelations()
     ,   fAidaImplants(new std::vector<AidaHit>)
     ,   fFrsAidaCorrelation(new TClonesArray("FrsAidaCorrelationData"))
 {
+    // CEJ: this can be done more cleanly with a lookup map ikik
+    // also need to work out nullptr check
+    for (int type = 0; type < fFrsGates.size(); type++)
+    {
+        if (strcmp(fFrsGates[type][0]->GetVarX(),"AoQ") == 0)
+        {
+            if (strcmp(fFrsGates[type][0]->GetVarY(),"Z") == 0)
+            {
+                cutID_Z_AoQ = fFrsGates[type];
+            }
+            else if (strcmp(fFrsGates[type][0]->GetVarY(), "x2") == 0)
+            {
+                cutID_x2AoQ = fFrsGates[type];
+            }
+            else if (strcmp(fFrsGates[type][0]->GetVarY(), "x4") == 0)
+            {
+                cutID_x4AoQ = fFrsGates[type];
+            }
+        }
+        else if (strcmp(fFrsGates[type][0]->GetVarX(), "Z") == 0)
+        {
+            if (strcmp(fFrsGates[type][0]->GetVarY(),"dEdeg") == 0)
+            {
+                cutID_dEdegZ = fFrsGates[type];
+            }
+        }
+        else if (strcmp(fFrsGates[type][0]->GetVarX(), "Z1") == 0)
+        {
+            if (strcmp(fFrsGates[type][0]->GetVarY(), "Z2") == 0)
+            {
+                cutID_Z_Z2 = fFrsGates[type];
+            }
+        }
+    }
 }
 
 FrsAidaCorrelations::FrsAidaCorrelations(const TString& name, Int_t verbose)
@@ -81,57 +115,79 @@ InitStatus FrsAidaCorrelations::Init()
 
     for (int dssd = 0; dssd < conf->DSSDs(); dssd++)
     {
-        for (int gate = 0; gate < FRS_GATES; gate++)
+
+        if (cutID_Z_AoQ[0] != nullptr)
         {
-            //hA_FRS_ZAoQ_implants_strip_xy
-            h2_AidaImplant_FRS_ZAoQgate_strip_xy[gate][dssd] = new TH2I(Form("h2_AidaImplant_FRS_ZAoQgate%d_strip_xy_dssd%d", gate, dssd + 1), Form("DSSD %d Implant hit pattern FRS ZAoQ Gate: %d", dssd + 1, gate), 128, 0, 128, 128, 0, 128);
-            //hA_FRS_ZAoQ_implants_e
-            h1_AidaImplant_FRS_ZAoQgate_e[gate][dssd] = new TH1I(Form("h1_AidaImplant_FRS_ZAoQgate%d_e_dssd%d", gate, dssd + 1), Form("DSSD %d Implant energy FRS ZAoQ Gate: %d", dssd + 1, gate), 1000, 0, 10000);
-            //hA_FRS_ZAoQ_implants_position
-            h2_AidaImplant_FRS_ZAoQgate_position[gate][dssd] = new TH2D(Form("h2_AidaImplant_FRS_ZAoQgate%d_position_dssd%d", gate, dssd + 1), Form("DSSD %d Implant position FRS ZAoQ Gate: %d", dssd + 1, gate), 128, -37.8, 37.8, 128, -37.8, 37.8);
-            //hA_FRS_Z1Z2_implants_strip_xy
-            h2_AidaImplant_FRS_Z1Z2gate_strip_xy[gate][dssd] = new TH2I(Form("h2_AidaImplant_FRS_Z1Z2gate%d_strip_xy_dssd%d", gate, dssd + 1), Form("DSSD %d Implant hit pattern FRS Z1Z2 Gate: %d", dssd + 1, gate), 128, 0, 128, 128, 0, 128);
-            //hA_FRS_Z1Z2_implants_position
-            h2_AidaImplant_FRS_Z1Z2gate_position[gate][dssd] = new TH2D(Form("h2_AidaImplant_FRS_Z1Z2gate%d_position_dssd%d", gate, dssd + 1), Form("DSSD %d Implant position FRS Z1Z2 Gate: %d", dssd + 1, gate), 128, -37.8, 37.8, 128, -37.8, 37.8);
-            //hA_FRS_Z1Z2_x2AoQ_implants_strip_xy
-            h2_AidaImplant_FRS_Z1Z2x2AoQgates_strip_xy[gate][dssd] = new TH2I(Form("h2_AidaImplant_FRS_Z1Z2x2AoQgates%d_strip_xy_dssd%d", gate, dssd + 1), Form("DSSD %d Implant hit pattern FRS Z1Zx2AoQ Gate: %d", dssd + 1, gate), 128, 0, 128, 128, 0, 128);
-            //hA_FRS_Z1Z2_x2AoQ_implants_position
-            h2_AidaImplant_FRS_Z1Z2x2AoQgates_position[gate][dssd] = new TH2D(Form("h2_AidaImplant_FRS_Z1Z2x2AoQgates%d_position_dssd%d", gate, dssd + 1), Form("DSSD %d Implant position FRS Z1Zx2AoQ Gate: %d", dssd + 1, gate), 128, -37.8, 37.8, 128, -37.8, 37.8);
-            //hA_FRS_Z1Z2_x2AoQ_implants_e
-            h1_AidaImplant_FRS_Z1Z2x2AoQgates_e[gate][dssd] = new TH1I(Form("h1_AidaImplant_FRS_Z1Z2x2AoQgates%d_e_dssd%d", gate, dssd + 1), Form("DSSD %d Implant energy FRS Z1Zx2AoQ Gate: %d", dssd + 1, gate), 1000, 0, 10000);
-            //hA_FRS_Z1Z2_x4AoQ_implants_strip_xy
-            h2_AidaImplant_FRS_Z1Z2x4AoQgates_strip_xy[gate][dssd] = new TH2I(Form("h2_AidaImplant_FRS_Z1Z2x4AoQgates%d_strip_xy_dssd%d", gate, dssd + 1), Form("DSSD %d Implant hit pattern FRS Z1Zx4AoQ Gate: %d", dssd + 1, gate), 128, 0, 128, 128, 0, 128);
-            //hA_FRS_Z1Z2_x4AoQ_implants_position
-            h2_AidaImplant_FRS_Z1Z2x4AoQgates_position[gate][dssd] = new TH2D(Form("h2_AidaImplant_FRS_Z1Z2x4AoQgates%d_position_dssd%d", gate, dssd + 1), Form("DSSD %d Implant position FRS Z1Zx4AoQ Gate: %d", dssd + 1, gate), 128, -37.8, 37.8, 128, -37.8, 37.8);
-            //hA_FRS_Z1Z2_x4AoQ_implants_e
-            h1_AidaImplant_FRS_Z1Z2x4AoQgates_e[gate][dssd] = new TH1I(Form("h1_AidaImplant_FRS_Z1Z2x4AoQgates%d_e_dssd%d", gate, dssd + 1), Form("DSSD %d Implant energy FRS Z1Zx4AoQ Gate: %d", dssd + 1, gate), 1000, 0, 10000);
+            for (int gate = 0; gate < cutID_Z_AoQ.size(); gate++)
+            {
+                //hA_FRS_ZAoQ_implants_strip_xy
+                h2_AidaImplant_FRS_ZAoQgate_strip_xy[dssd].emplace_back(new TH2I(Form("h2_AidaImplant_FRS_ZAoQgate%d_strip_xy_dssd%d", gate, dssd + 1), Form("DSSD %d Implant hit pattern FRS ZAoQ Gate: %d", dssd + 1, gate), 128, 0, 128, 128, 0, 128));
+                //hA_FRS_ZAoQ_implants_e
+                h1_AidaImplant_FRS_ZAoQgate_e[dssd].emplace_back(new TH1I(Form("h1_AidaImplant_FRS_ZAoQgate%d_e_dssd%d", gate, dssd + 1), Form("DSSD %d Implant energy FRS ZAoQ Gate: %d", dssd + 1, gate), 1000, 0, 10000));
+                //hA_FRS_ZAoQ_implants_position
+                h2_AidaImplant_FRS_ZAoQgate_position[dssd].emplace_back(new TH2D(Form("h2_AidaImplant_FRS_ZAoQgate%d_position_dssd%d", gate, dssd + 1), Form("DSSD %d Implant position FRS ZAoQ Gate: %d", dssd + 1, gate), 128, -37.8, 37.8, 128, -37.8, 37.8));
 
-            // ----- same again but stopped ---
+                // ----- STOPPED -------
+                //hA_FRS_ZAoQ_implants_strip_xy
+                h2_AidaImplant_FRS_ZAoQgate_strip_xy_stopped[dssd].emplace_back(new TH2I(Form("h2_AidaImplant_FRS_ZAoQgate%d_strip_xy_stopped_dssd%d", gate, dssd + 1), Form("DSSD %d Implant (stopped) hit pattern FRS ZAoQ Gate: %d", dssd + 1, gate), 128, 0, 128, 128, 0, 128));
+                //hA_FRS_ZAoQ_implants_e
+                h1_AidaImplant_FRS_ZAoQgate_e_stopped[dssd].emplace_back(new TH1I(Form("h1_AidaImplant_FRS_ZAoQgate%d_e_stopped_dssd%d", gate, dssd + 1), Form("DSSD %d Implant (stopped) energy FRS ZAoQ Gate: %d", dssd + 1, gate), 1000, 0, 10000));
+                //hA_FRS_ZAoQ_implants_position
+                h2_AidaImplant_FRS_ZAoQgate_position_stopped[dssd].emplace_back(new TH2D(Form("h2_AidaImplant_FRS_ZAoQgate%d_position_stopped_dssd%d", gate, dssd + 1), Form("DSSD %d Implant (stopped) position FRS ZAoQ Gate: %d", dssd + 1, gate), 128, -37.8, 37.8, 128, -37.8, 37.8));
+            }
+        }
 
-            //hA_FRS_ZAoQ_implants_strip_xy
-            h2_AidaImplant_FRS_ZAoQgate_strip_xy_stopped[gate][dssd] = new TH2I(Form("h2_AidaImplant_FRS_ZAoQgate%d_strip_xy_stopped_dssd%d", gate, dssd + 1), Form("DSSD %d Implant (stopped) hit pattern FRS ZAoQ Gate: %d", dssd + 1, gate), 128, 0, 128, 128, 0, 128);
-            //hA_FRS_ZAoQ_implants_e
-            h1_AidaImplant_FRS_ZAoQgate_e_stopped[gate][dssd] = new TH1I(Form("h1_AidaImplant_FRS_ZAoQgate%d_e_stopped_dssd%d", gate, dssd + 1), Form("DSSD %d Implant (stopped) energy FRS ZAoQ Gate: %d", dssd + 1, gate), 1000, 0, 10000);
-            //hA_FRS_ZAoQ_implants_position
-            h2_AidaImplant_FRS_ZAoQgate_position_stopped[gate][dssd] = new TH2D(Form("h2_AidaImplant_FRS_ZAoQgate%d_position_stopped_dssd%d", gate, dssd + 1), Form("DSSD %d Implant (stopped) position FRS ZAoQ Gate: %d", dssd + 1, gate), 128, -37.8, 37.8, 128, -37.8, 37.8);
-            //hA_FRS_Z1Z2_implants_strip_xy
-            h2_AidaImplant_FRS_Z1Z2gate_strip_xy_stopped[gate][dssd] = new TH2I(Form("h2_AidaImplant_FRS_Z1Z2gate%d_strip_xy_stopped_dssd%d", gate, dssd + 1), Form("DSSD %d Implant (stopped) hit pattern FRS Z1Z2 Gate: %d", dssd + 1, gate), 128, 0, 128, 128, 0, 128);
-            //hA_FRS_Z1Z2_implants_position
-            h2_AidaImplant_FRS_Z1Z2gate_position_stopped[gate][dssd] = new TH2D(Form("h2_AidaImplant_FRS_Z1Z2gate%d_position_stopped_dssd%d", gate, dssd + 1), Form("DSSD %d Implant (stopped) position FRS Z1Z2 Gate: %d", dssd + 1, gate), 128, -37.8, 37.8, 128, -37.8, 37.8);
-            //hA_FRS_Z1Z2_x2AoQ_implants_strip_xy
-            h2_AidaImplant_FRS_Z1Z2x2AoQgates_strip_xy_stopped[gate][dssd] = new TH2I(Form("h2_AidaImplant_FRS_Z1Z2x2AoQgates%d_strip_xy_stopped_dssd%d", gate, dssd + 1), Form("DSSD %d Implant (stopped) hit pattern FRS Z1Zx2AoQ Gate: %d", dssd + 1, gate), 128, 0, 128, 128, 0, 128);
-            //hA_FRS_Z1Z2_x2AoQ_implants_position
-            h2_AidaImplant_FRS_Z1Z2x2AoQgates_position_stopped[gate][dssd] = new TH2D(Form("h2_AidaImplant_FRS_Z1Z2x2AoQgates%d_position_stopped_dssd%d", gate, dssd + 1), Form("DSSD %d Implant (stopped) position FRS Z1Zx2AoQ Gate: %d", dssd + 1, gate), 128, -37.8, 37.8, 128, -37.8, 37.8);
-            //hA_FRS_Z1Z2_x2AoQ_implants_e
-            h1_AidaImplant_FRS_Z1Z2x2AoQgates_e_stopped[gate][dssd] = new TH1I(Form("h1_AidaImplant_FRS_Z1Z2x2AoQgates%d_e_stopped_dssd%d", gate, dssd + 1), Form("DSSD %d Implant (stopped) energy FRS Z1Zx2AoQ Gate: %d", dssd + 1, gate), 1000, 0, 10000);
-            //hA_FRS_Z1Z2_x4AoQ_implants_strip_xy
-            h2_AidaImplant_FRS_Z1Z2x4AoQgates_strip_xy_stopped[gate][dssd] = new TH2I(Form("h2_AidaImplant_FRS_Z1Z2x4AoQgates%d_strip_xy_stopped_dssd%d", gate, dssd + 1), Form("DSSD %d Implant (stopped) hit pattern FRS Z1Zx4AoQ Gate: %d", dssd + 1, gate), 128, 0, 128, 128, 0, 128);
-            //hA_FRS_Z1Z2_x4AoQ_implants_position
-            h2_AidaImplant_FRS_Z1Z2x4AoQgates_position_stopped[gate][dssd] = new TH2D(Form("h2_AidaImplant_FRS_Z1Z2x4AoQgates%d_position_stopped_dssd%d", gate, dssd + 1), Form("DSSD %d Implant (stopped) position FRS Z1Zx4AoQ Gate: %d", dssd + 1, gate), 128, -37.8, 37.8, 128, -37.8, 37.8);
-            //hA_FRS_Z1Z2_x4AoQ_implants_e
-            h1_AidaImplant_FRS_Z1Z2x4AoQgates_e_stopped[gate][dssd] = new TH1I(Form("h1_AidaImplant_FRS_Z1Z2x4AoQgates%d_e_stopped_dssd%d", gate, dssd + 1), Form("DSSD %d Implant (stopped) energy FRS Z1Zx4AoQ Gate: %d", dssd + 1, gate), 1000, 0, 10000);
+        if (cutID_Z_Z2[0] != nullptr)
+        {
+            for (int gate = 0; gate < cutID_Z_Z2.size(); gate++)
+            {
+                //hA_FRS_Z1Z2_implants_strip_xy
+                h2_AidaImplant_FRS_Z1Z2gate_strip_xy[dssd].emplace_back(new TH2I(Form("h2_AidaImplant_FRS_Z1Z2gate%d_strip_xy_dssd%d", gate, dssd + 1), Form("DSSD %d Implant hit pattern FRS Z1Z2 Gate: %d", dssd + 1, gate), 128, 0, 128, 128, 0, 128));
+                //hA_FRS_Z1Z2_implants_position
+                h2_AidaImplant_FRS_Z1Z2gate_position[dssd].emplace_back(new TH2D(Form("h2_AidaImplant_FRS_Z1Z2gate%d_position_dssd%d", gate, dssd + 1), Form("DSSD %d Implant position FRS Z1Z2 Gate: %d", dssd + 1, gate), 128, -37.8, 37.8, 128, -37.8, 37.8));
 
+                // ------- STOPPED --------------
+                //hA_FRS_Z1Z2_implants_strip_xy
+                h2_AidaImplant_FRS_Z1Z2gate_strip_xy_stopped[dssd].emplace_back(new TH2I(Form("h2_AidaImplant_FRS_Z1Z2gate%d_strip_xy_stopped_dssd%d", gate, dssd + 1), Form("DSSD %d Implant (stopped) hit pattern FRS Z1Z2 Gate: %d", dssd + 1, gate), 128, 0, 128, 128, 0, 128));
+                //hA_FRS_Z1Z2_implants_position
+                h2_AidaImplant_FRS_Z1Z2gate_position_stopped[dssd].emplace_back(new TH2D(Form("h2_AidaImplant_FRS_Z1Z2gate%d_position_stopped_dssd%d", gate, dssd + 1), Form("DSSD %d Implant (stopped) position FRS Z1Z2 Gate: %d", dssd + 1, gate), 128, -37.8, 37.8, 128, -37.8, 37.8));
 
+                if (cutID_x2AoQ[0] != nullptr)
+                {
+                    //hA_FRS_Z1Z2_x2AoQ_implants_strip_xy
+                    h2_AidaImplant_FRS_Z1Z2x2AoQgates_strip_xy[dssd].emplace_back(new TH2I(Form("h2_AidaImplant_FRS_Z1Z2x2AoQgates%d_strip_xy_dssd%d", gate, dssd + 1), Form("DSSD %d Implant hit pattern FRS Z1Zx2AoQ Gate: %d", dssd + 1, gate), 128, 0, 128, 128, 0, 128));
+                    //hA_FRS_Z1Z2_x2AoQ_implants_position
+                    h2_AidaImplant_FRS_Z1Z2x2AoQgates_position[dssd].emplace_back(new TH2D(Form("h2_AidaImplant_FRS_Z1Z2x2AoQgates%d_position_dssd%d", gate, dssd + 1), Form("DSSD %d Implant position FRS Z1Zx2AoQ Gate: %d", dssd + 1, gate), 128, -37.8, 37.8, 128, -37.8, 37.8));
+                    //hA_FRS_Z1Z2_x2AoQ_implants_e
+                    h1_AidaImplant_FRS_Z1Z2x2AoQgates_e[dssd].emplace_back(new TH1I(Form("h1_AidaImplant_FRS_Z1Z2x2AoQgates%d_e_dssd%d", gate, dssd + 1), Form("DSSD %d Implant energy FRS Z1Zx2AoQ Gate: %d", dssd + 1, gate), 1000, 0, 10000));
+
+                    // ------------- STOPPED ----------------
+                    //hA_FRS_Z1Z2_x2AoQ_implants_strip_xy
+                    h2_AidaImplant_FRS_Z1Z2x2AoQgates_strip_xy_stopped[dssd].emplace_back(new TH2I(Form("h2_AidaImplant_FRS_Z1Z2x2AoQgates%d_strip_xy_stopped_dssd%d", gate, dssd + 1), Form("DSSD %d Implant (stopped) hit pattern FRS Z1Zx2AoQ Gate: %d", dssd + 1, gate), 128, 0, 128, 128, 0, 128));
+                    //hA_FRS_Z1Z2_x2AoQ_implants_position
+                    h2_AidaImplant_FRS_Z1Z2x2AoQgates_position_stopped[dssd].emplace_back(new TH2D(Form("h2_AidaImplant_FRS_Z1Z2x2AoQgates%d_position_stopped_dssd%d", gate, dssd + 1), Form("DSSD %d Implant (stopped) position FRS Z1Zx2AoQ Gate: %d", dssd + 1, gate), 128, -37.8, 37.8, 128, -37.8, 37.8));
+                    //hA_FRS_Z1Z2_x2AoQ_implants_e
+                    h1_AidaImplant_FRS_Z1Z2x2AoQgates_e_stopped[dssd].emplace_back(new TH1I(Form("h1_AidaImplant_FRS_Z1Z2x2AoQgates%d_e_stopped_dssd%d", gate, dssd + 1), Form("DSSD %d Implant (stopped) energy FRS Z1Zx2AoQ Gate: %d", dssd + 1, gate), 1000, 0, 10000));
+                }
+
+                if (cutID_x4AoQ[0] != nullptr)
+                {
+                    //hA_FRS_Z1Z2_x4AoQ_implants_strip_xy
+                    h2_AidaImplant_FRS_Z1Z2x4AoQgates_strip_xy[dssd].emplace_back(new TH2I(Form("h2_AidaImplant_FRS_Z1Z2x4AoQgates%d_strip_xy_dssd%d", gate, dssd + 1), Form("DSSD %d Implant hit pattern FRS Z1Zx4AoQ Gate: %d", dssd + 1, gate), 128, 0, 128, 128, 0, 128));
+                    //hA_FRS_Z1Z2_x4AoQ_implants_position
+                    h2_AidaImplant_FRS_Z1Z2x4AoQgates_position[dssd].emplace_back(new TH2D(Form("h2_AidaImplant_FRS_Z1Z2x4AoQgates%d_position_dssd%d", gate, dssd + 1), Form("DSSD %d Implant position FRS Z1Zx4AoQ Gate: %d", dssd + 1, gate), 128, -37.8, 37.8, 128, -37.8, 37.8));
+                    //hA_FRS_Z1Z2_x4AoQ_implants_e
+                    h1_AidaImplant_FRS_Z1Z2x4AoQgates_e[dssd].emplace_back(new TH1I(Form("h1_AidaImplant_FRS_Z1Z2x4AoQgates%d_e_dssd%d", gate, dssd + 1), Form("DSSD %d Implant energy FRS Z1Zx4AoQ Gate: %d", dssd + 1, gate), 1000, 0, 10000));
+
+                    // ---------- STOPPED
+                    //hA_FRS_Z1Z2_x4AoQ_implants_strip_xy
+                    h2_AidaImplant_FRS_Z1Z2x4AoQgates_strip_xy_stopped[dssd].emplace_back(new TH2I(Form("h2_AidaImplant_FRS_Z1Z2x4AoQgates%d_strip_xy_stopped_dssd%d", gate, dssd + 1), Form("DSSD %d Implant (stopped) hit pattern FRS Z1Zx4AoQ Gate: %d", dssd + 1, gate), 128, 0, 128, 128, 0, 128));
+                    //hA_FRS_Z1Z2_x4AoQ_implants_position
+                    h2_AidaImplant_FRS_Z1Z2x4AoQgates_position_stopped[dssd].emplace_back(new TH2D(Form("h2_AidaImplant_FRS_Z1Z2x4AoQgates%d_position_stopped_dssd%d", gate, dssd + 1), Form("DSSD %d Implant (stopped) position FRS Z1Zx4AoQ Gate: %d", dssd + 1, gate), 128, -37.8, 37.8, 128, -37.8, 37.8));
+                    //hA_FRS_Z1Z2_x4AoQ_implants_e
+                    h1_AidaImplant_FRS_Z1Z2x4AoQgates_e_stopped[dssd].emplace_back(new TH1I(Form("h1_AidaImplant_FRS_Z1Z2x4AoQgates%d_e_stopped_dssd%d", gate, dssd + 1), Form("DSSD %d Implant (stopped) energy FRS Z1Zx4AoQ Gate: %d", dssd + 1, gate), 1000, 0, 10000));
+                }
+            }
         }
     }
 
