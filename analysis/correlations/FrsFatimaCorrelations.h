fb320e667423535616c43748f62af04150d1679b
cej25 <c.jones@gsi.de>
2024-02-25 01:28:15 +0100

    FrsFatimaCorrelations - Prompt. Method for loading Correlations config file.

--- analysis/correlations/FrsFatimaCorrelations.h
+++ analysis/correlations/FrsFatimaCorrelations.h
@@ -1,9 +1,103 @@
 #ifndef FrsFatimaCorrelations_H
 #define FrsFatimaCorrelations_H
 
+#include "FairTask.h"
 
+#include "FrsHitData.h"
+#include "FatimaTwinpeaksCalData.h"
+#include "CorrelationsMap.h"
 
+#include "TFolder.h"
+#include "TH1.h"
+#include "TH2.h"
+#include "TClonesArray.h"
+#include "TCutG.h"
+#include "TCutGGates.h"
+#include <vector>
 
+class TClonesArray;
+class TFolder;
+class TH1F;
+class TH1D;
+class TH2D;
+class EventHeader;
+class TCutG;
+class TCutGGates;
+class FrsHitData;
+class FatimaTwinpeaksCalData;
+
+class FrsFatimaCorrelations : public FairTask
+{
+    public:
+        FrsFatimaCorrelations(std::vector<TCutGGates*> fFrsGates,
+                            TCutGGates* fFatimaPromptGates,
+                            CorrelationsMap* fCorrel);
+        virtual ~FrsFatimaCorrelations();
+
+        virtual InitStatus Init();
+
+        virtual void Exec(Option_t* option);
+
+        virtual void FinishEvent();
+        
+        virtual void FinishTask();
+
+    private:
+
+        Int_t fNEvents;
+        EventHeader* header;
+
+        TClonesArray* fHitFatimaTwinpeaksArray;
+        TClonesArray* fHitFrsArray;
+
+        // Gates
+        std::vector<TCutG*> cutID_Z_AoQ;
+        std::vector<TCutG*> cutID_Z_Z2;
+        std::vector<TCutG*> cutID_x2AoQ;
+        std::vector<TCutG*> cutID_x4AoQ;
+        std::vector<TCutG*> cutID_dEdegZ;
+        
+        std::vector<TCutG*> cutFatima_EdT;
+
+        CorrelationsMap* Correl;
+
+        // Folders
+        TFolder* folder_frs_fatima_corr;
+        TFolder* folder_frs_fatima_corr_ZAoQgated;
+        TFolder* folder_frs_fatima_corr_Z1Z2x2AoQgated;
+        TFolder* folder_frs_fatima_corr_Z1Z2x4AoQgated;
+
+        // Histograms
+        TH1D* h1_FRS_Fatima_LT1_start_stop;
+        TH1D* h1_FRS_Fatima_LT1_start_stop_ns;
+        TH1D* h1_FRS_Fatima_LT2_start_stop;
+        TH1D* h1_FRS_Fatima_LT2_start_stop_ns;
+        TH1D* h1_FRS_Fatima_LT3_start_stop;
+        TH1D* h1_FRS_Fatima_LT3_start_stop_ns;
+        TH1D* h1_FRS_Fatima_LT1_stop_start;
+        TH1D* h1_FRS_Fatima_LT1_stop_start_ns;
+        TH1D* h1_FRS_Fatima_LT2_stop_start;
+        TH1D* h1_FRS_Fatima_LT2_stop_start_ns;
+        TH1D* h1_FRS_Fatima_LT3_stop_start;
+        TH1D* h1_FRS_Fatima_LT3_stop_start_ns;
+        std::vector<TH1F*> h1_FRS_FatimaE_ZAoQgate;
+        std::vector<TH2D*> h2_FRS_FatimaEvsT_ZAoQgate;
+        std::vector<TH2D*> h2_FRS_FatimaE1vsE2_ZAoQgate;
+        std::vector<TH1F*> h1_FRS_FatimaE_Z1Z2x2AoQgate;
+        std::vector<TH2D*> h2_FRS_FatimaEvsT_Z1Z2x2AoQgate;
+        std::vector<TH2D*> h2_FRS_FatimaE1vsE2_Z1Z2x2AoQgate;
+        std::vector<TH1F*> h1_FRS_FatimaE_Z1Z2x4AoQgate;
+        std::vector<TH2D*> h2_FRS_FatimaEvsT_Z1Z2x4AoQgate;
+        std::vector<TH2D*> h2_FRS_FatimaE1vsE2_Z1Z2x4AoQgate;
+
+        // Variables
+        Long64_t Fatima_time_mins;
+
+        
+
+    public:
+        ClassDef(FrsFatimaCorrelations, 1)
+};
 
 
 #endif
---
+++ c4source/base/CorrelationsMap.cxx
@@ -0,0 +1,77 @@
+#include "CorrelationsMap.h"
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+
+CorrelationsMap::CorrelationsMap(std::string& filename)
+{
+    std::ifstream file(filename);
+    std::string line;
+
+    while (std::getline(file, line))
+    {
+        if (line.empty() || line[0] == '#') continue;
+
+        std::istringstream iss(line);
+        std::string firstWord;
+        iss >> firstWord;
+
+        if (!firstWord.empty())
+        {
+            int value;
+            std::vector<int> values;
+            while (iss >> value) values.push_back(value);
+
+            if (!values.empty())
+            {
+                if (firstWord == "FRS_AIDA") (*this)["FRS-AIDA"] = values;
+                else if (firstWord == "FRS_FATIMA_PROMPT") (*this)["FRS-Fatima Prompt"] = values;
+                else if (firstWord == "FRS_FATIMA_LONG") (*this)["FRS-Fatima Long"] = values;
+                else if (firstWord == "FRS_FATIMA_LT_ANALYSIS") (*this)["FRS-Fatima Lifetime Analysis"] = values;
+                else if (firstWord == "FRS_BPLAST") (*this)["FRS-bPlast"] = values;
+                else if (firstWord == "FRS_GERMANIUM_PROMPT") (*this)["FRS-Germanium Prompt"] = values;
+                else if (firstWord == "FRS_GERMANIUM_LONG") (*this)["FRS-Germanium Long"] = values;
+                else if (firstWord == "SPILL_OFF_GAMMAS") (*this)["Spill-Off Gammas"] = values;
+                else if (firstWord == "BETA_DELAY_GAMMAS") (*this)["Beta-Delayed Gammas"] = values;
+                else if (firstWord == "BETA_DELAY_GAMMAS_LT_ANALYSIS") (*this)["Beta-Delayed Gamma Lifetime Analysis"] = values;
+                else if (firstWord == "FRS_AIDA_WR") (*this)["FRS-AIDA WR Gate"] = values;
+                else if (firstWord == "FRS_FATIMA_WR") (*this)["FRS-Fatima WR Gate"] = values;
+                else if (firstWord == "FRS_BPLAST_WR") (*this)["FRS-bPlast WR Gate"] = values;
+                else if (firstWord == "FRS_GERMANIUM_WR") (*this)["FRS-Germanium WR Gate"] = values;
+                else if (firstWord == "AIDA_DECAYS_GERMANIUM_WR") (*this)["AIDA(Decays)-Germanium WR Gate"] = values;
+                else if (firstWord == "AIDA_DECAYS_FATIMA_WR") (*this)["AIDA(Decays)-Fatima WR Gate"] = values;
+                else if (firstWord == "AIDA_IMPLANTS_BPLAST_WR") (*this)["AIDA(Implants)-bPlast WR Gate"] = values;
+                else if (firstWord == "AIDA_DECAYS_BPLAST_WR") (*this)["AIDA(Decays)-bPlast WR Gate"] = values;
+                else if (firstWord == "BPLAST_FATIMA_VME_WR") (*this)["bPlast-Fatima(VME) WR Gate"] = values;
+                else if (firstWord == "BPLAST_FATIMA_TAMEX_WR") (*this)["bPlast-Fatima(TAMEX) WR Gate"] = values;
+                else if (firstWord == "BPLAST_GERMANIUM_WR") (*this)["bPlast-Germanium WR Gate"] = values;
+                else if (firstWord == "FATIMA_VME_FATIMA_TAMEX_WR") (*this)["Fatima(VME)-Fatima(TAMEX) WR Gate"] = values;
+                else if (firstWord == "FATIMA_VME_GERMANIUM_WR") (*this)["Fatima(VME)-Germanium WR Gate"] = values;
+                else if (firstWord == "FATIMA_TAMEX_GERMANIUM_WR") (*this)["Fatima(TAMEX)-Germanium WR Gate"] = values;
+                else if (firstWord == "FRS_AIDA_TM") (*this)["FRS-AIDA TM Gate"] = values;
+                else if (firstWord == "AIDA_GERMANIUM_TM") (*this)["AIDA-Germanium TM Gate"] = values;
+                else if (firstWord == "AIDA_FATIMA_VME_TM") (*this)["AIDA-Fatima(VME) TM Gate"] = values;
+                else if (firstWord == "AIDA_FATIMA_TAMEX_TM") (*this)["AIDA-Fatima(TAMEX) TM Gate"] = values;
+                else if (firstWord == "AIDA_BPLAST_TM") (*this)["AIDA-bPlast TM Gate"] = values;
+                else if (firstWord == "GAMMA_GAMMA_GERMANIUM_TIME") (*this)["Germanium Gamma-Gamma dT Gate"] = values;
+                else if (firstWord == "FRS_GERMANIUM_LONG_DT") (*this)["FRS-Germanium Long dT Gate"] = values;
+                else if (firstWord == "GAMMA_GAMMA_FATIMA_TIME") (*this)["Fatima Gamma-Gamma dT Gate"] = values;
+                else if (firstWord == "FRS_FATIMA_LONG_DT") (*this)["FRS-Fatima Long dT Gate"] = values;
+                else if (firstWord == "LT_START_STOP_1") (*this)["Lifetime 1 Start-Stop Energies"] = values;
+                else if (firstWord == "LT_START_STOP_2") (*this)["Lifetime 2 Start-Stop Energies"] = values;
+                else if (firstWord == "LT_START_STOP 3") (*this)["Lifetime 3 Start-Stop Energies"] = values;
+                else if (firstWord == "LT_START_STOP_PID") (*this)["Start-Stop Analysis PID"] = values;
+                else if (firstWord == "IMPLANT_DECAY_DT") (*this)["Implant-Decay dT Gate"] = values;
+                else if (firstWord == "AIDA_FB_DT_DE") (*this)["AIDA Front-Back Conditions"] = values;
+                else if (firstWord == "AIDA_MAX_DECAY_E") (*this)["AIDA Max Decay-Energy Gates"] = values;
+                else if (firstWord == "BPLAST_TOT_GATE") (*this)["bPlast ToT Gate"] = values;
+                else if (firstWord == "BDG_START_STOP_1") (*this)["Beta-Delayed Gamma Lifetime 1 Start-Stop Energies"] = values;
+                else if (firstWord == "BDG_START_STOP_2") (*this)["Beta-Delayed Gamma Lifetime 2 Start-Stop Energies"] = values;
+                else if (firstWord == "BDG_PID") (*this)["Beta-Delayed Gamma PID"] = values;
+            }
+        } 
+        
+    }
+
+}
