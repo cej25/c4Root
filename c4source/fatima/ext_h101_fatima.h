960ecd11008fe83c135746f251a81dc13a500c0d
Johan Emil Linnestad Larsson <jlarsson@lxg3159.gsi.de>
2024-03-11 11:32:20 +0100

    changes

--- c4source/fatima/ext_h101_fatima.h
+++ c4source/fatima/ext_h101_fatima.h
@@ -51,6 +51,28 @@ typedef struct EXT_STR_h101_fatima_t
   uint32_t fatima_tamex2time_channelv[1024 EXT_STRUCT_CTRL(fatima_tamex2time_channel)] /* [0,65535] */;
   uint32_t fatima_tamex2time_epoch /* [0,1024] */;
   uint32_t fatima_tamex2time_epochv[1024 EXT_STRUCT_CTRL(fatima_tamex2time_epoch)] /* [-1,-1] */;
+  uint32_t fatima_tamex3event_size /* [-1,-1] */;
+  uint32_t fatima_tamex3time_coarse /* [0,1024] */;
+  uint32_t fatima_tamex3time_coarsev[1024 EXT_STRUCT_CTRL(fatima_tamex3time_coarse)] /* [0,65535] */;
+  uint32_t fatima_tamex3time_fine /* [0,1024] */;
+  uint32_t fatima_tamex3time_finev[1024 EXT_STRUCT_CTRL(fatima_tamex3time_fine)] /* [0,65535] */;
+  uint32_t fatima_tamex3time_edge /* [0,1024] */;
+  uint32_t fatima_tamex3time_edgev[1024 EXT_STRUCT_CTRL(fatima_tamex3time_edge)] /* [0,65535] */;
+  uint32_t fatima_tamex3time_channel /* [0,1024] */;
+  uint32_t fatima_tamex3time_channelv[1024 EXT_STRUCT_CTRL(fatima_tamex3time_channel)] /* [0,65535] */;
+  uint32_t fatima_tamex3time_epoch /* [0,1024] */;
+  uint32_t fatima_tamex3time_epochv[1024 EXT_STRUCT_CTRL(fatima_tamex3time_epoch)] /* [-1,-1] */;
+  uint32_t fatima_tamex4event_size /* [-1,-1] */;
+  uint32_t fatima_tamex4time_coarse /* [0,1024] */;
+  uint32_t fatima_tamex4time_coarsev[1024 EXT_STRUCT_CTRL(fatima_tamex4time_coarse)] /* [0,65535] */;
+  uint32_t fatima_tamex4time_fine /* [0,1024] */;
+  uint32_t fatima_tamex4time_finev[1024 EXT_STRUCT_CTRL(fatima_tamex4time_fine)] /* [0,65535] */;
+  uint32_t fatima_tamex4time_edge /* [0,1024] */;
+  uint32_t fatima_tamex4time_edgev[1024 EXT_STRUCT_CTRL(fatima_tamex4time_edge)] /* [0,65535] */;
+  uint32_t fatima_tamex4time_channel /* [0,1024] */;
+  uint32_t fatima_tamex4time_channelv[1024 EXT_STRUCT_CTRL(fatima_tamex4time_channel)] /* [0,65535] */;
+  uint32_t fatima_tamex4time_epoch /* [0,1024] */;
+  uint32_t fatima_tamex4time_epochv[1024 EXT_STRUCT_CTRL(fatima_tamex4time_epoch)] /* [-1,-1] */;
   uint32_t fatima_ts_subsystem_id /* [0,65535] */;
   uint32_t fatima_ts_t1 /* [0,65535] */;
   uint32_t fatima_ts_t2 /* [0,65535] */;
@@ -84,7 +106,7 @@ typedef struct EXT_STR_h101_fatima_onion_t
     uint32_t time_channelv[1024 /* time_channel */];
     uint32_t time_epoch;
     uint32_t time_epochv[1024 /* time_epoch */];
-  } fatima_tamex[2];
+  } fatima_tamex[4];
   uint32_t fatima_ts_subsystem_id;
   uint32_t fatima_ts_t[4];
   uint32_t fatima_trigger_window_post_trig_ns;
@@ -165,6 +187,72 @@ typedef struct EXT_STR_h101_fatima_onion_t
   EXT_STR_ITEM_INFO2_ZZP(ok,si,offset,struct_t,printerr,\
                      fatima_tamex2time_epochv,        UINT32,\
                     "fatima_tamex2time_epochv",        "fatima_tamex2time_epoch",0/*flags*/); \
+  EXT_STR_ITEM_INFO2    (ok,si,offset,struct_t,printerr,\
+                     fatima_tamex3event_size,         UINT32,\
+                    "fatima_tamex3event_size",0/*flags*/); \
+  EXT_STR_ITEM_INFO2_LIM(ok,si,offset,struct_t,printerr,\
+                     fatima_tamex3time_coarse,        UINT32,\
+                    "fatima_tamex3time_coarse",1024,0/*flags*/); \
+  EXT_STR_ITEM_INFO2_ZZP(ok,si,offset,struct_t,printerr,\
+                     fatima_tamex3time_coarsev,       UINT32,\
+                    "fatima_tamex3time_coarsev",       "fatima_tamex3time_coarse",0/*flags*/); \
+  EXT_STR_ITEM_INFO2_LIM(ok,si,offset,struct_t,printerr,\
+                     fatima_tamex3time_fine,          UINT32,\
+                    "fatima_tamex3time_fine",1024,0/*flags*/); \
+  EXT_STR_ITEM_INFO2_ZZP(ok,si,offset,struct_t,printerr,\
+                     fatima_tamex3time_finev,         UINT32,\
+                    "fatima_tamex3time_finev",         "fatima_tamex3time_fine",0/*flags*/); \
+  EXT_STR_ITEM_INFO2_LIM(ok,si,offset,struct_t,printerr,\
+                     fatima_tamex3time_edge,          UINT32,\
+                    "fatima_tamex3time_edge",1024,0/*flags*/); \
+  EXT_STR_ITEM_INFO2_ZZP(ok,si,offset,struct_t,printerr,\
+                     fatima_tamex3time_edgev,         UINT32,\
+                    "fatima_tamex3time_edgev",         "fatima_tamex3time_edge",0/*flags*/); \
+  EXT_STR_ITEM_INFO2_LIM(ok,si,offset,struct_t,printerr,\
+                     fatima_tamex3time_channel,       UINT32,\
+                    "fatima_tamex3time_channel",1024,0/*flags*/); \
+  EXT_STR_ITEM_INFO2_ZZP(ok,si,offset,struct_t,printerr,\
+                     fatima_tamex3time_channelv,      UINT32,\
+                    "fatima_tamex3time_channelv",      "fatima_tamex3time_channel",0/*flags*/); \
+  EXT_STR_ITEM_INFO2_LIM(ok,si,offset,struct_t,printerr,\
+                     fatima_tamex3time_epoch,         UINT32,\
+                    "fatima_tamex3time_epoch",1024,0/*flags*/); \
+  EXT_STR_ITEM_INFO2_ZZP(ok,si,offset,struct_t,printerr,\
+                     fatima_tamex3time_epochv,        UINT32,\
+                    "fatima_tamex3time_epochv",        "fatima_tamex3time_epoch",0/*flags*/); \
+  EXT_STR_ITEM_INFO2    (ok,si,offset,struct_t,printerr,\
+                     fatima_tamex4event_size,         UINT32,\
+                    "fatima_tamex4event_size",0/*flags*/); \
+  EXT_STR_ITEM_INFO2_LIM(ok,si,offset,struct_t,printerr,\
+                     fatima_tamex4time_coarse,        UINT32,\
+                    "fatima_tamex4time_coarse",1024,0/*flags*/); \
+  EXT_STR_ITEM_INFO2_ZZP(ok,si,offset,struct_t,printerr,\
+                     fatima_tamex4time_coarsev,       UINT32,\
+                    "fatima_tamex4time_coarsev",       "fatima_tamex4time_coarse",0/*flags*/); \
+  EXT_STR_ITEM_INFO2_LIM(ok,si,offset,struct_t,printerr,\
+                     fatima_tamex4time_fine,          UINT32,\
+                    "fatima_tamex4time_fine",1024,0/*flags*/); \
+  EXT_STR_ITEM_INFO2_ZZP(ok,si,offset,struct_t,printerr,\
+                     fatima_tamex4time_finev,         UINT32,\
+                    "fatima_tamex4time_finev",         "fatima_tamex4time_fine",0/*flags*/); \
+  EXT_STR_ITEM_INFO2_LIM(ok,si,offset,struct_t,printerr,\
+                     fatima_tamex4time_edge,          UINT32,\
+                    "fatima_tamex4time_edge",1024,0/*flags*/); \
+  EXT_STR_ITEM_INFO2_ZZP(ok,si,offset,struct_t,printerr,\
+                     fatima_tamex4time_edgev,         UINT32,\
+                    "fatima_tamex4time_edgev",         "fatima_tamex4time_edge",0/*flags*/); \
+  EXT_STR_ITEM_INFO2_LIM(ok,si,offset,struct_t,printerr,\
+                     fatima_tamex4time_channel,       UINT32,\
+                    "fatima_tamex4time_channel",1024,0/*flags*/); \
+  EXT_STR_ITEM_INFO2_ZZP(ok,si,offset,struct_t,printerr,\
+                     fatima_tamex4time_channelv,      UINT32,\
+                    "fatima_tamex4time_channelv",      "fatima_tamex4time_channel",0/*flags*/); \
+  EXT_STR_ITEM_INFO2_LIM(ok,si,offset,struct_t,printerr,\
+                     fatima_tamex4time_epoch,         UINT32,\
+                    "fatima_tamex4time_epoch",1024,0/*flags*/); \
+  EXT_STR_ITEM_INFO2_ZZP(ok,si,offset,struct_t,printerr,\
+                     fatima_tamex4time_epochv,        UINT32,\
+                    "fatima_tamex4time_epochv",        "fatima_tamex4time_epoch",0/*flags*/); \
   EXT_STR_ITEM_INFO2_LIM(ok,si,offset,struct_t,printerr,\
                      fatima_ts_subsystem_id,          UINT32,\
                     "fatima_ts_subsystem_id",65535,0/*flags*/); \
