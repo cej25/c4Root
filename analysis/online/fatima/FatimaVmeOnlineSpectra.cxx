6ff6883c08e28fd6740cedb3105cc03d543caa5c
cej25 <c.jones@gsi.de>
2024-03-13 02:51:03 +0100

    started online spectra task for fatima-vme, wrong branch - oops.

---
+++ analysis/online/fatima/FatimaVmeOnlineSpectra.cxx
@@ -0,0 +1,23 @@
+// FairRoot
+#include "FairLogger.h"
+#include "FairRootManager.h"
+#include "FairRunAna.h"
+#include "FairRunOnline.h"
+#include "FairRuntimeDb.h"
+
+// c4
+#include "FatimaVmeOnlineSpectra.h"
+#include "EventHeader.h"
+#include "FatimaVmeCalData.h"
+
+#include "c4Logger.h"
+
+#include "TCanvas.h"
+#include "TClonesArray.h"
+#include "TFolder.h"
+#include "TH1F.h"
+#include "TH2F.h"
+#include "THttpServer.h"
+#include "TMath.h"
+#include "TFile.h"
+#include "TRandom.h"
