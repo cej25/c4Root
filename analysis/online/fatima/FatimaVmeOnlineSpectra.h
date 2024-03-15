37d1cf3df9fa6faf68fb6588f4f5cf5c8daf7cad
cej25 <c.jones@gsi.de>
2024-03-15 17:04:16 +0100

    bug-fixing fatima-vme tdc t

--- analysis/online/fatima/FatimaVmeOnlineSpectra.h
+++ analysis/online/fatima/FatimaVmeOnlineSpectra.h
@@ -10,6 +10,7 @@ class EventHeader;
 
 class TH1I;
 class TH1D;
+class TH2D;
 
 class FatimaVmeOnlineSpectra : public FairTask
 {
@@ -38,20 +39,36 @@ class FatimaVmeOnlineSpectra : public FairTask
 
         // Folders
         TFolder* folder_fatima_vme;
+        TFolder* folder_stats_vme;
         TFolder* folder_raw_vme;
+        TFolder* folder_cal_vme;
         TFolder* folder_raw_energy;
         TFolder* folder_raw_time;
+        TFolder* folder_cal_energy;
+        TFolder* folder_cal_time;
 
         // Canvases
+        TCanvas* c_FatVME_E;
+        TCanvas* c_FatVME_RawE;
+        TCanvas* c_FatVME_RawT;
 
         // Histograms
-        TH1I* h1_FatVME_QDCMult;
-        TH1I* h1_FatVME_TDCMult;
         // these should be vectors based on detector config.
         TH1D* h1_FatVME_RawE[36];
-        TH1D* h1_FatVME_E[36];
         TH1D* h1_FatVME_RawT[36];
-    
+        TH1D* h1_FatVME_E[36];
+        TH1D* h1_TDC_dt_refCh1[36];
+        TH1D* h1_TDC_dT_refSC41L[36];
+        TH1D* h1_FatVME_E_Sum;
+        TH1D* h1_FatVME_E_Sum_Large; // large range
+        TH1D* h1_FatVME_QDC_HitPattern;
+        TH1D* h1_FatVME_TDC_HitPattern;
+        TH1I* h1_FatVME_QDCMult;
+        TH1I* h1_FatVME_TDCMult;
+        TH2D* h1_FatVME_EvsT;
+        TH1D* h1_FatVME_Time;
+
+
     public:
         ClassDef(FatimaVmeOnlineSpectra, 1);
 };
