6091d9e8c241106fa3a6b914e398dec4fa5fcf26
Rooitier <rooitier@gmail.com>
2024-03-19 11:24:02 +0100

    some whiterabbit changes

--- unpack/scripts/gen_struct_header.py
+++ unpack/scripts/gen_struct_header.py
@@ -4,9 +4,9 @@ import subprocess as sb
 #should create the correct struct files, add systems to systems list and run. 
 
 #add the systems you want here: bplast,fatima,etc
-systems = ["bplast","fatima"]
+systems = ["bplast"]
 
-ucesb_exec = "NovTest"
+ucesb_exec = "onlybPlast"
 
 
 for system in systems:
