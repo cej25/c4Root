eed79ed7ce1bd18e637f0b1ac35d97dc550ac1e5
cej25 <c.jones@gsi.de>
2024-03-22 01:44:50 +0100

    adding tpat reader for wr and tpat. lets see if it actually works when we have real data

---
+++ c4source/frs/FrsTpatReader.h
@@ -0,0 +1,60 @@
+#ifndef FrsTpatReader_H
+#define FrsTpatReader_H
+
+#include "FrsTpatData.h"
+#include "c4Reader.h"
+#include <Rtypes.h>
+#include <vector>
+
+extern "C"
+{
+    #include "ext_h101_frstpat.h"
+}
+
+class TClonesArray;
+class FrsTpatData;
+
+struct EXT_STR_h101_frstpat_t;
+typedef struct EXT_STR_h101_frstpat_t EXT_STR_h101_frstpat;
+typedef struct EXT_STR_h101_frstpat_onion_t EXT_STR_h101_frstpat_onion;
+class ext_data_struct_info;
+
+class FrsTpatReader : public c4Reader
+{
+    public:
+        FrsTpatReader(EXT_STR_h101_frstpat_onion*, size_t);
+
+        virtual ~FrsTpatReader();
+
+        virtual Bool_t Init(ext_data_struct_info*) override;
+
+        virtual Bool_t Read() override;
+
+        //void ZeroArrays();
+        //void ClearVectors();
+
+        virtual void Reset() override;
+
+        void SetOnline(Bool_t option) { fOnline = option; }
+
+    private:
+
+        unsigned int fNEvent;
+        
+        EXT_STR_h101_frstpat_onion* fData;
+
+        size_t fOffset;
+
+        Bool_t fOnline;
+
+        TClonesArray* fArray;
+
+        uint64_t wr_t;
+        uint16_t tpat;
+
+    
+    public:
+        ClassDefOverride(FrsTpatReader, 1);
+};
+
+#endif
