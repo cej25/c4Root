f39ad95ff47ed81881ad46a32e4a1f97a7088d75
cej25 <c.jones@gsi.de>
2024-03-29 16:29:48 +0100

    fix aida bug

--- c4source/aida/c4AidaReader.cxx
+++ c4source/aida/c4AidaReader.cxx
@@ -40,8 +40,8 @@ AidaReader::~AidaReader()
 Bool_t AidaReader::Init(ext_data_struct_info* a_struct_info)
 {
     Int_t ok;
-    
-    EXT_STR_h101_AIDA_ITEMS_INFO(ok, *a_struct_info, fOffset, EXT_STR_h101_aida, 0);
+
+    EXT_STR_h101_aida_ITEMS_INFO(ok, *a_struct_info, fOffset, EXT_STR_h101_aida, 0);
 
     if (!ok)
     {
