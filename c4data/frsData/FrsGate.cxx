a6952995fdcf49f99a729dd2043e4e968f7d1b05
cej25 <c.jones@gsi.de>
2024-04-15 17:07:47 +0200

    changed FrsNearline to accept FrsGate class

--- c4data/frsData/FrsGate.cxx
+++ c4data/frsData/FrsGate.cxx
@@ -30,12 +30,14 @@ FrsGate::FrsGate(std::string gn, std::string file_name)
 };
 
 
-std::string FrsGate::GetName(){
+std::string FrsGate::GetName()
+{
     return gate_name;
 };
 
 
-bool FrsGate::PassedGate(double Z, double Z2, double x2, double x4, double AoQ, double dEdeg){
+bool FrsGate::PassedGate(double Z, double Z2, double x2, double x4, double AoQ, double dEdeg)
+{
     bool passed_gate_cut_Z_AoQ = false;
     bool passed_gate_cut_Z_Z2 = false;
     bool passed_gate_cut_x2_AoQ = false;
@@ -70,5 +72,50 @@ bool FrsGate::PassedGate(double Z, double Z2, double x2, double x4, double AoQ, 
 
     return passed_gate_cut_Z_AoQ && passed_gate_cut_Z_Z2 && passed_gate_cut_x2_AoQ && passed_gate_cut_x4_AoQ && passed_gate_cut_dEdeg_Z;
 };
+
+bool FrsGate::Passed_ZvsAoQ(double Z, double AoQ)
+{
+    bool passed_gate_cut_Z_AoQ = false;
+    if (cut_Z_AoQ != nullptr) passed_gate_cut_Z_AoQ = cut_Z_AoQ->IsInside(AoQ, Z);
+    else passed_gate_cut_Z_AoQ = true;
+    
+    return passed_gate_cut_Z_AoQ;
+}
+
+bool FrsGate::Passed_ZvsZ2(double Z, double Z2)
+{
+    bool passed_gate_cut_Z_Z2 = false;
+    if (cut_Z_Z2 != nullptr) passed_gate_cut_Z_Z2 = cut_Z_Z2->IsInside(Z, Z2);
+    else passed_gate_cut_Z_Z2 = true;
+    
+    return passed_gate_cut_Z_Z2;
+}
+
+bool FrsGate::Passed_x2vsAoQ(double x2, double AoQ)
+{
+    bool passed_gate_cut_x2_AoQ = false;
+    if (cut_x2_AoQ != nullptr) passed_gate_cut_x2_AoQ = cut_x2_AoQ->IsInside(AoQ, x2);
+    else passed_gate_cut_x2_AoQ = true;
+    
+    return passed_gate_cut_x2_AoQ;
+}
+
+bool FrsGate::Passed_x4vsAoQ(double x4, double AoQ)
+{
+    bool passed_gate_cut_x4_AoQ = false;
+    if (cut_x4_AoQ != nullptr) passed_gate_cut_x4_AoQ = cut_x4_AoQ->IsInside(AoQ, x4);
+    else passed_gate_cut_x4_AoQ = true;
+    
+    return passed_gate_cut_x4_AoQ;
+}
+
+bool FrsGate::Passed_dEdegvsZ(double Z, double dEdeg)
+{
+    bool passed_gate_cut_dEdeg_Z = false;
+    if (cut_dEdeg_Z != nullptr) passed_gate_cut_dEdeg_Z = cut_dEdeg_Z->IsInside(Z, dEdeg);
+    else passed_gate_cut_dEdeg_Z = true;
+    
+    return passed_gate_cut_dEdeg_Z;
+}
 
 ClassImp(FrsGate)
