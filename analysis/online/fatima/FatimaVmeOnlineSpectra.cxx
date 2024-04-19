98cc78a27fedceddaa5eb0349cab081faea85117
cej25 <c.jones@gsi.de>
2024-04-19 13:59:20 +0200

    local changes, cleanup for online tasks

--- analysis/online/fatima/FatimaVmeOnlineSpectra.cxx
+++ analysis/online/fatima/FatimaVmeOnlineSpectra.cxx
@@ -71,6 +71,7 @@ InitStatus FatimaVmeOnlineSpectra::Init()
     dir_stats_vme = dir_fatima_vme->mkdir("Stats");
     dir_raw_vme = dir_fatima_vme->mkdir("Raw Spectra");
     dir_cal_vme = dir_fatima_vme->mkdir("Calibrated Spectra");
+    dir_residuals = dir_fatima_vme->mkdir("Residual Signals");
 
 
     // Setting histogram sizes
@@ -89,7 +90,6 @@ InitStatus FatimaVmeOnlineSpectra::Init()
     dir_raw_energy = dir_raw_vme->mkdir("Raw Energy");
     dir_cal_energy = dir_cal_vme->mkdir("Calibrated Energy");
     dir_raw_time = dir_raw_vme->mkdir("Raw Time");
-    dir_cal_time = dir_cal_vme->mkdir("Calibrated Time");
 
     dir_raw_energy->cd();
     c_FatVME_RawE = new TCanvas("c_FatVME_RawE","Fatima VME Raw Energies", 650, 350);
@@ -129,7 +129,7 @@ InitStatus FatimaVmeOnlineSpectra::Init()
     c_FatVME_RawT->cd(0);
     dir_raw_time->Append(c_FatVME_RawT);
 
-    dir_tdc_dt = dir_cal_vme->mkdir("Time Differences");
+    dir_tdc_dt = dir_fatima_vme->mkdir("Time Differences");
     dir_dt_sc41 = dir_tdc_dt->mkdir("SC41L dT");
     dir_dt_ch1 = dir_tdc_dt->mkdir("Ch1 dT");
 
@@ -157,13 +157,15 @@ InitStatus FatimaVmeOnlineSpectra::Init()
     c_FatVME_dTrefCh1->cd(0);
     dir_dt_ch1->Append(c_FatVME_dTrefCh1);
     
-    dir_fatima_vme->cd();
+    dir_residuals->cd();
     h1_FatVME_time_machine_undelayed = new TH1D("h1_FatVME_time_machine_undelayed", "Time Machine Undelayed - FATIMA VME", 2000, 0, 40000);
     h1_FatVME_time_machine_delayed = new TH1D("h1_FatVME_time_machine_delayed", "Time Machine Delayed - FATIMA VME", 2000, 0, 40000);
     //h1_FatVME_sc41l = new TH1D("h1_FatVME_sc41l", "SC41L E - FATIMA VME", 2000, 0, 40000);
     //h1_FatVME_sc41r = new TH1D("h1_FatVME_sc41r", "SC41R E - FATIMA VME", 2000, 0, 40000);
     h1_FatVME_sc41l_time = new TH1D("h1_FatVME_sc41l_time", "SC41L T - FATIMA VME", 5000, -1e6, 7e7);
     h1_FatVME_sc41r_time = new TH1D("h1_FatVME_sc41r_time", "SC41R T - FATIMA VME", 5000, -1e6, 7e7);
+
+    dir_fatima_vme->cd();
 
     run->GetHttpServer()->RegisterCommand("Reset_FATIMA_VME_Histo", Form("/Objects/%s/->Reset_Histo()", GetName()));
     run->GetHttpServer()->RegisterCommand("Snapshot_FATIMA_VME_Histo", Form("/Objects/%s/->Snapshot_Histo()", GetName()));
