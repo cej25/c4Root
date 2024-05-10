1403cdd2472f2d543e3d8a1319b746e8f8dc72e6
cej25 <c.jones@gsi.de>
2024-05-10 17:02:44 +0200

    FIMP channel mapping -made up for now..

--- c4source/fimp/FimpRaw2Cal.h
+++ c4source/fimp/FimpRaw2Cal.h
@@ -41,7 +41,7 @@ class FimpRaw2Cal : public FairTask
 
         uint64_t wr_t;
 
-        // detector mapping
+        std::map<int, std::pair<std::pair<int, int>, std::string>> detector_mapping;
 
 
     public:
