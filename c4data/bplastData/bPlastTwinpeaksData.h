c5479168ab5bd4a13e326d593a37c5b4a120a3fd
cej25 <c.jones@gsi.de>
2024-06-13 16:45:05 +0200

    adding trigger info into degas, fatima, bgo, bplast data structures

--- c4data/bplastData/bPlastTwinpeaksData.h
+++ c4data/bplastData/bPlastTwinpeaksData.h
@@ -10,6 +10,7 @@ class bPlastTwinpeaksData : public TObject
         bPlastTwinpeaksData();
 
         bPlastTwinpeaksData(
+        uint8_t trig,
         uint16_t board_id,
         uint32_t ch_ID,
         uint32_t lead_epoch_counter,
@@ -26,7 +27,7 @@ class bPlastTwinpeaksData : public TObject
         virtual ~bPlastTwinpeaksData() {}
 
         // Getters
-
+        inline const uint8_t Get_trigger() const { return ftrig; }
         inline const uint16_t Get_board_id() const {return fboard_id; }
         inline const uint32_t Get_ch_ID() const {return fch_ID; }
 
@@ -45,6 +46,7 @@ class bPlastTwinpeaksData : public TObject
 
 
         // Setters
+        void Set_trigger(uint8_t v) {ftrig = v; }
         void Set_board_id(uint16_t v){fboard_id = v;}
         void Set_ch_ID(uint32_t v){fch_ID = v;}
         void Set_lead_epoch_counter(uint32_t v){flead_epoch_counter = v;}
@@ -58,7 +60,7 @@ class bPlastTwinpeaksData : public TObject
 
     protected:
         // Data items
-
+        uint8_t ftrig;
         uint16_t fboard_id;
         uint32_t fch_ID;
 
