20305b6080b4d7f737502afc0d41a57d51803c83
Rooitier <rooitier@gmail.com>
2025-02-19 09:59:32 +0100

    updated util

--- macros/util/make_frs_gates_seq_tchain.cpp
+++ macros/util/make_frs_gates_seq_tchain.cpp
@@ -1,14 +1,15 @@
 
-bool Gate_Z_AoQ(TTree * evt, bool bool_Z_AoQ, bool  bool_Z_Z2, bool  bool_x2_AoQ, bool  bool_x4_AoQ, bool bool_dEdeg_Z){
+bool Gate_Z_AoQ(TTree * evt, bool bool_Z_AoQ, bool  bool_Z_Z2, bool  bool_x2_AoQ, bool  bool_x4_AoQ, bool bool_dEdeg_Z, bool bool_sci42E_Z){
     
     TString cuts;
 
-    if (bool_Z_Z2) cuts = cuts + "cut_Z_Z2+";
-    if (bool_x2_AoQ) cuts = cuts + "cut_x2_AoQ+";
-    if (bool_x4_AoQ) cuts = cuts + "cut_x4_AoQ+";
-    if (bool_dEdeg_Z) cuts = cuts + "cut_dEdeg_Z+";
+    if (bool_Z_Z2) cuts = cuts + "cut_Z_Z2 && ";
+    if (bool_x2_AoQ) cuts = cuts + "cut_x2_AoQ && ";
+    if (bool_x4_AoQ) cuts = cuts + "cut_x4_AoQ && ";
+    if (bool_dEdeg_Z) cuts = cuts + "cut_dEdeg_Z && ";
+    if (bool_sci42E_Z) cuts = cuts + "cut_sci42E_Z && ";
 
-    if (cuts.EndsWith("+")) cuts = cuts(0,cuts.Length()-1);
+    if (cuts.EndsWith(" && ")) cuts = cuts(0,cuts.Length()-4);
     std::cout << "Drawing with the condition: " << cuts << std::endl;
     
 
@@ -21,7 +22,7 @@ bool Gate_Z_AoQ(TTree * evt, bool bool_Z_AoQ, bool  bool_Z_Z2, bool  bool_x2_AoQ
     gPad->WaitPrimitive("CUTG");
     TCutG * cut_Z_AoQ = (TCutG*)gROOT->FindObject("CUTG");
     if (cut_Z_AoQ == nullptr){
-        std::cout << "No Z vs AoQ gate given. Continuing." << std::endl;
+        std::cout << "No gate given. Continuing." << std::endl;
         return false;
     }else{
         cut_Z_AoQ = (TCutG*)cut_Z_AoQ->Clone("cut_Z_AoQ");
@@ -34,17 +35,18 @@ bool Gate_Z_AoQ(TTree * evt, bool bool_Z_AoQ, bool  bool_Z_Z2, bool  bool_x2_AoQ
     }
 };
 
-bool Gate_Z_Z2(TTree * evt, bool bool_Z_AoQ, bool  bool_Z_Z2, bool  bool_x2_AoQ, bool  bool_x4_AoQ, bool bool_dEdeg_Z){
+bool Gate_Z_Z2(TTree * evt, bool bool_Z_AoQ, bool  bool_Z_Z2, bool  bool_x2_AoQ, bool  bool_x4_AoQ, bool bool_dEdeg_Z, bool bool_sci42E_Z){
     
     TString cuts;
 
-    if (bool_Z_AoQ) cuts = cuts + "cut_Z_AoQ+";
-    if (bool_x2_AoQ) cuts = cuts + "cut_x2_AoQ+";
-    if (bool_x4_AoQ) cuts = cuts + "cut_x4_AoQ+";
-    if (bool_dEdeg_Z) cuts = cuts + "cut_dEdeg_Z+";
+    if (bool_Z_AoQ) cuts = cuts + "cut_Z_AoQ && ";
+    if (bool_x2_AoQ) cuts = cuts + "cut_x2_AoQ && ";
+    if (bool_x4_AoQ) cuts = cuts + "cut_x4_AoQ && ";
+    if (bool_dEdeg_Z) cuts = cuts + "cut_dEdeg_Z && ";
+    if (bool_sci42E_Z) cuts = cuts + "cut_sci42E_Z && ";
 
 
-    if (cuts.EndsWith("+")) cuts = cuts(0,cuts.Length()-1);
+    if (cuts.EndsWith(" && ")) cuts = cuts(0,cuts.Length()-4);
     std::cout << "Drawing with the condition: " << cuts << std::endl;
     
     
@@ -57,7 +59,7 @@ bool Gate_Z_Z2(TTree * evt, bool bool_Z_AoQ, bool  bool_Z_Z2, bool  bool_x2_AoQ,
     gPad->WaitPrimitive("CUTG");
     TCutG * cut_Z_Z2 = (TCutG*)gROOT->FindObject("CUTG");
     if (cut_Z_Z2 == nullptr){
-        std::cout << "No Z vs AoQ gate given. Continuing." << std::endl;
+        std::cout << "No gate given. Continuing." << std::endl;
         return false;
     }else{
         cut_Z_Z2 = (TCutG*)cut_Z_Z2->Clone("cut_Z_Z2");
@@ -70,17 +72,18 @@ bool Gate_Z_Z2(TTree * evt, bool bool_Z_AoQ, bool  bool_Z_Z2, bool  bool_x2_AoQ,
 }
 
 
-bool Gate_x2_AoQ(TTree * evt, bool bool_Z_AoQ, bool  bool_Z_Z2, bool  bool_x2_AoQ, bool  bool_x4_AoQ, bool bool_dEdeg_Z){
+bool Gate_x2_AoQ(TTree * evt, bool bool_Z_AoQ, bool  bool_Z_Z2, bool  bool_x2_AoQ, bool  bool_x4_AoQ, bool bool_dEdeg_Z, bool bool_sci42E_Z){
     
     TString cuts;
 
-    if (bool_Z_AoQ) cuts = cuts + "cut_Z_AoQ+";
-    if (bool_Z_Z2) cuts = cuts + "cut_Z_Z2+";
-    if (bool_x4_AoQ) cuts = cuts + "cut_x4_AoQ+";
-    if (bool_dEdeg_Z) cuts = cuts + "cut_dEdeg_Z+";
+    if (bool_Z_AoQ) cuts = cuts + "cut_Z_AoQ && ";
+    if (bool_Z_Z2) cuts = cuts + "cut_Z_Z2 && ";
+    if (bool_x4_AoQ) cuts = cuts + "cut_x4_AoQ && ";
+    if (bool_dEdeg_Z) cuts = cuts + "cut_dEdeg_Z && ";
+    if (bool_sci42E_Z) cuts = cuts + "cut_sci42E_Z && ";
 
 
-    if (cuts.EndsWith("+")) cuts = cuts(0,cuts.Length()-1);
+    if (cuts.EndsWith(" && ")) cuts = cuts(0,cuts.Length()-4);
     std::cout << "Drawing with the condition: " << cuts << std::endl;
     
     
@@ -93,7 +96,7 @@ bool Gate_x2_AoQ(TTree * evt, bool bool_Z_AoQ, bool  bool_Z_Z2, bool  bool_x2_Ao
     gPad->WaitPrimitive("CUTG");
     TCutG * cut_x2_AoQ = (TCutG*)gROOT->FindObject("CUTG");
     if (cut_x2_AoQ == nullptr){
-        std::cout << "No Z vs AoQ gate given. Continuing." << std::endl;
+        std::cout << "No gate given. Continuing." << std::endl;
         return false;
     }else{
         cut_x2_AoQ = (TCutG*)cut_x2_AoQ->Clone("cut_x2_AoQ");
@@ -105,17 +108,18 @@ bool Gate_x2_AoQ(TTree * evt, bool bool_Z_AoQ, bool  bool_Z_Z2, bool  bool_x2_Ao
     }
 }
 
-bool Gate_x4_AoQ(TTree * evt, bool bool_Z_AoQ, bool  bool_Z_Z2, bool  bool_x2_AoQ, bool  bool_x4_AoQ, bool bool_dEdeg_Z){
+bool Gate_x4_AoQ(TTree * evt, bool bool_Z_AoQ, bool  bool_Z_Z2, bool  bool_x2_AoQ, bool  bool_x4_AoQ, bool bool_dEdeg_Z, bool bool_sci42E_Z){
     
     TString cuts;
 
-    if (bool_Z_AoQ) cuts = cuts + "cut_Z_AoQ+";
-    if (bool_Z_Z2) cuts = cuts + "cut_Z_Z2+";
-    if (bool_x2_AoQ) cuts = cuts + "cut_x2_AoQ+";
-    if (bool_dEdeg_Z) cuts = cuts + "cut_dEdeg_Z+";
+    if (bool_Z_AoQ) cuts = cuts + "cut_Z_AoQ && ";
+    if (bool_Z_Z2) cuts = cuts + "cut_Z_Z2 && ";
+    if (bool_x2_AoQ) cuts = cuts + "cut_x2_AoQ && ";
+    if (bool_dEdeg_Z) cuts = cuts + "cut_dEdeg_Z && ";
+    if (bool_sci42E_Z) cuts = cuts + "cut_sci42E_Z && ";
 
 
-    if (cuts.EndsWith("+")) cuts = cuts(0,cuts.Length()-1);
+    if (cuts.EndsWith(" && ")) cuts = cuts(0,cuts.Length()-4);
     std::cout << "Drawing with the condition: " << cuts << std::endl;
     
     
@@ -128,7 +132,7 @@ bool Gate_x4_AoQ(TTree * evt, bool bool_Z_AoQ, bool  bool_Z_Z2, bool  bool_x2_Ao
     gPad->WaitPrimitive("CUTG");
     TCutG * cut_x4_AoQ = (TCutG*)gROOT->FindObject("CUTG");
     if (cut_x4_AoQ == nullptr){
-        std::cout << "No Z vs AoQ gate given. Continuing." << std::endl;
+        std::cout << "No gate given. Continuing." << std::endl;
         return false;
     }else{
         cut_x4_AoQ = (TCutG*)cut_x4_AoQ->Clone("cut_x4_AoQ");
@@ -140,17 +144,18 @@ bool Gate_x4_AoQ(TTree * evt, bool bool_Z_AoQ, bool  bool_Z_Z2, bool  bool_x2_Ao
     }
 }
 
-bool Gate_dEdeg_Z(TTree * evt, bool bool_Z_AoQ, bool  bool_Z_Z2, bool  bool_x2_AoQ, bool  bool_x4_AoQ, bool bool_dEdeg_Z){
+bool Gate_dEdeg_Z(TTree * evt, bool bool_Z_AoQ, bool  bool_Z_Z2, bool  bool_x2_AoQ, bool  bool_x4_AoQ, bool bool_dEdeg_Z, bool bool_sci42E_Z){
     
     TString cuts;
 
-    if (bool_Z_AoQ) cuts = cuts + "cut_Z_AoQ+";
-    if (bool_Z_Z2) cuts = cuts + "cut_Z_Z2+";
-    if (bool_x2_AoQ) cuts = cuts + "cut_x2_AoQ+";
-    if (bool_x4_AoQ) cuts = cuts + "cut_x4_AoQ+";
+    if (bool_Z_AoQ) cuts = cuts + "cut_Z_AoQ && ";
+    if (bool_Z_Z2) cuts = cuts + "cut_Z_Z2 && ";
+    if (bool_x2_AoQ) cuts = cuts + "cut_x2_AoQ && ";
+    if (bool_x4_AoQ) cuts = cuts + "cut_x4_AoQ && ";
+    if (bool_sci42E_Z) cuts = cuts + "cut_sci42E_Z && ";
 
 
-    if (cuts.EndsWith("+")) cuts = cuts(0,cuts.Length()-1);
+    if (cuts.EndsWith(" && ")) cuts = cuts(0,cuts.Length()-4);
     std::cout << "Drawing with the condition: " << cuts << std::endl;
     
     
@@ -163,7 +168,7 @@ bool Gate_dEdeg_Z(TTree * evt, bool bool_Z_AoQ, bool  bool_Z_Z2, bool  bool_x2_A
     gPad->WaitPrimitive("CUTG");
     TCutG * cut_dEdeg_Z = (TCutG*)gROOT->FindObject("CUTG");
     if (cut_dEdeg_Z == nullptr){
-        std::cout << "No Z vs AoQ gate given. Continuing." << std::endl;
+        std::cout << "No gate given. Continuing." << std::endl;
         return false;
     }else{
         cut_dEdeg_Z = (TCutG*)cut_dEdeg_Z->Clone("cut_dEdeg_Z");
@@ -175,25 +180,57 @@ bool Gate_dEdeg_Z(TTree * evt, bool bool_Z_AoQ, bool  bool_Z_Z2, bool  bool_x2_A
     }
 }
 
+bool Gate_sci42E_Z(TChain * evt, bool bool_Z_AoQ, bool  bool_Z_Z2, bool  bool_x2_AoQ, bool  bool_x4_AoQ, bool bool_dEdeg_Z, bool bool_sci42E_Z){
+    
+    TString cuts;
 
-void make_frs_gates_seq(TString infilename){
+    if (bool_Z_AoQ) cuts = cuts + "cut_Z_AoQ && ";
+    if (bool_Z_Z2) cuts = cuts + "cut_Z_Z2 && ";
+    if (bool_x2_AoQ) cuts = cuts + "cut_x2_AoQ && ";
+    if (bool_x4_AoQ) cuts = cuts + "cut_x4_AoQ && ";
+    if (bool_dEdeg_Z) cuts = cuts + "cut_dEdeg_Z && ";
+    
 
-    TFile * infile = TFile::Open(infilename,"READ");
 
-    if (!infile->IsOpen()){
-        std::cout << "Failed on file open." << std::endl;
-        return;
+    if (cuts.EndsWith(" && ")) cuts = cuts(0,cuts.Length()-4);
+    std::cout << "Drawing with the condition: " << cuts << std::endl;
+    
+    
+    evt->Draw("FrsHitData.fID_z41:FrsHitData.fsci_e_42>>h2_sci42E_Z(1000,0,2500,1000,60,70)",cuts);
+    
+    TH2F * h2_sci42E_Z = (TH2F*)gROOT->FindObject("h2_sci42E_Z");
+    
+    h2_sci42E_Z->SetTitle("Draw sci42E vs. Z gate.");
+    h2_sci42E_Z->Draw("COLZ");
+    gPad->WaitPrimitive("CUTG");
+    TCutG * cut_sci42E_Z = (TCutG*)gROOT->FindObject("CUTG");
+    if (cut_sci42E_Z == nullptr){
+        std::cout << "No gate given. Continuing." << std::endl;
+        return false;
+    }else{
+        cut_sci42E_Z = (TCutG*)cut_sci42E_Z->Clone("cut_sci42E_Z");
+        gROOT->FindObject("CUTG")->Delete();
+        cut_sci42E_Z->SetVarY("FrsHitData.fID_z41");
+        cut_sci42E_Z->SetVarX("FrsHitData.fsci_e_42");
+        std::cout << "Created gate: " << cut_sci42E_Z->GetName() << std::endl;
+        return true;
     }
+}
 
-    TTree * evt;
-    infile->GetObject("evt",evt);
+
+void make_frs_gates_seq_tchain(TString infilename){
+
+
+    TChain * evt = new TChain("evt");
+
+    evt->Add(infilename);
 
     bool bool_Z_AoQ = false;
     bool bool_Z_Z2 = false;
     bool bool_x2_AoQ = false;
     bool bool_x4_AoQ = false;
     bool bool_dEdeg_Z = false;
-
+    bool bool_sci42E_Z = false;
 
 
     TString gatename;
@@ -204,24 +241,27 @@ void make_frs_gates_seq(TString infilename){
     bool making_gates = true;
     while (making_gates){
         int gate;
-        std::cout << "Please choose parameters to gate - (1) AoQ vs Z, (2) Z1 vs Z2, (3) x2 vs AoQ, (4) x4 vs AoQ or (5) dEdeg vs Z:" << std::endl << ">>>";
+        std::cout << "Please choose parameters to gate - (1) AoQ vs Z, (2) Z1 vs Z2, (3) x2 vs AoQ, (4) x4 vs AoQ or (5) dEdeg vs Z or (6) sci42 E vs Z:" << std::endl << ">>>";
         std::cin >> gate;
 
         switch (gate){
             case 1:
-                bool_Z_AoQ = Gate_Z_AoQ(evt, bool_Z_AoQ, bool_Z_Z2, bool_x2_AoQ, bool_x4_AoQ, bool_dEdeg_Z);
+                bool_Z_AoQ = Gate_Z_AoQ(evt, bool_Z_AoQ, bool_Z_Z2, bool_x2_AoQ, bool_x4_AoQ, bool_dEdeg_Z, bool_sci42E_Z);
             break;
             case 2:
-                bool_Z_Z2 = Gate_Z_Z2(evt, bool_Z_AoQ, bool_Z_Z2, bool_x2_AoQ, bool_x4_AoQ, bool_dEdeg_Z);
+                bool_Z_Z2 = Gate_Z_Z2(evt, bool_Z_AoQ, bool_Z_Z2, bool_x2_AoQ, bool_x4_AoQ, bool_dEdeg_Z, bool_sci42E_Z);
             break;
             case 3:
-                bool_x2_AoQ = Gate_x2_AoQ(evt, bool_Z_AoQ, bool_Z_Z2, bool_x2_AoQ, bool_x4_AoQ, bool_dEdeg_Z);
+                bool_x2_AoQ = Gate_x2_AoQ(evt, bool_Z_AoQ, bool_Z_Z2, bool_x2_AoQ, bool_x4_AoQ, bool_dEdeg_Z, bool_sci42E_Z);
             break;
             case 4:
-                bool_x4_AoQ = Gate_x4_AoQ(evt, bool_Z_AoQ, bool_Z_Z2, bool_x2_AoQ, bool_x4_AoQ, bool_dEdeg_Z);
+                bool_x4_AoQ = Gate_x4_AoQ(evt, bool_Z_AoQ, bool_Z_Z2, bool_x2_AoQ, bool_x4_AoQ, bool_dEdeg_Z, bool_sci42E_Z);
             break;
             case 5:
-                bool_dEdeg_Z = Gate_dEdeg_Z(evt, bool_Z_AoQ, bool_Z_Z2, bool_x2_AoQ, bool_x4_AoQ, bool_dEdeg_Z);
+                bool_dEdeg_Z = Gate_dEdeg_Z(evt, bool_Z_AoQ, bool_Z_Z2, bool_x2_AoQ, bool_x4_AoQ, bool_dEdeg_Z, bool_sci42E_Z);
+            break;
+            case 6:
+                bool_sci42E_Z = Gate_sci42E_Z(evt, bool_Z_AoQ, bool_Z_Z2, bool_x2_AoQ, bool_x4_AoQ, bool_dEdeg_Z, bool_sci42E_Z);
             break;
 
             default:
@@ -233,15 +273,15 @@ void make_frs_gates_seq(TString infilename){
     }
 
 
-    TFile * outfile =  TFile::Open(gatename+".root","RECREATE");
+    TFile * outfile =  TFile::Open(gatename + ".root","RECREATE");
 
     if (bool_Z_AoQ) gROOT->FindObject("cut_Z_AoQ")->Write();
     if (bool_Z_Z2) gROOT->FindObject("cut_Z_Z2")->Write();
     if (bool_x2_AoQ) gROOT->FindObject("cut_x2_AoQ")->Write();
     if (bool_x4_AoQ) gROOT->FindObject("cut_x4_AoQ")->Write();
     if (bool_dEdeg_Z)gROOT->FindObject("cut_dEdeg_Z")->Write();
+    if (bool_sci42E_Z)gROOT->FindObject("cut_sci42E_Z")->Write();
     
     outfile->Close();
     
 };
-
