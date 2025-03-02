4c8983f3311184b55a70e0b74a86ef84d96ad4a1
cej25 <c.jones@gsi.de>
2025-03-02 18:58:56 +0100

    minor fix

--- analysis/correlations/FrsGermaniumCorrelations.cxx
+++ analysis/correlations/FrsGermaniumCorrelations.cxx
@@ -365,10 +365,13 @@ void FrsGermaniumCorrelations::Exec(Option_t* option)
         std::vector<Float_t> dEdeg_z41_mhtdc = multihitItem.Get_ID_dEdeg_z41_mhtdc();
 
         // For now we just take first hit, not really sure how to check PID with multihits honestly
-        ID_AoQ_s2s4 = AoQ_s2s4_mhtdc.at(0);
-        ID_z41 = z41_mhtdc.at(0);
-        ID_z42 = z42_mhtdc.at(0);
-        ID_dEdegZ41 = dEdeg_z41_mhtdc.at(0);
+        if (ID_AoQ_s2s4->size() > 0)
+        {
+            ID_AoQ_s2s4 = AoQ_s2s4_mhtdc.at(0);
+            ID_z41 = z41_mhtdc.at(0);
+            ID_z42 = z42_mhtdc.at(0);
+            ID_dEdegZ41 = dEdeg_z41_mhtdc.at(0);
+        }
     }
 
     // Well we can do something more strict but for now ZvsAoQ is fine
