74b932e83305afb2fd53709cff68fcb35095213c
eligand <e.m.gandolfo@gsi.de>
2025-04-30 16:05:29 +0200

    FrsGate.cxx wip

--- c4data/frsData/FrsGate.cxx
+++ c4data/frsData/FrsGate.cxx
@@ -10,21 +10,38 @@ FrsGate::FrsGate(std::string gn, std::string file_name)
     gate_name = gn;
 
     TFile* infile = TFile::Open(TString(file_name),"READ");
+    // cut_Z41_AoQs2s4 = (TCutG*)infile->Get("cut_Z41_AoQs2s4");
+    // cut_Z41_Z42 = (TCutG*)infile->Get("cut_Z41_Z4");
+    // cut_x2_AoQs2s4 = (TCutG*)infile->Get("cut_x2_AoQs2s4");
+    // cut_x4_AoQs2s4 = (TCutG*)infile->Get("cut_x4_AoQs2s4");
+    // cut_dEdegZ41_Z41 = (TCutG*)infile->Get("cut_dEdeg_Z41");
+    // cut_sci42E_Z41 = (TCutG*)infile->Get("cut_sci42E_Z41");
+    cut_Z21_AoQs1s2 = (TCutG*)infile->Get("cut_Z21_AoQs1s2");
     cut_Z41_AoQs2s4 = (TCutG*)infile->Get("cut_Z41_AoQs2s4");
-    cut_Z41_Z42 = (TCutG*)infile->Get("cut_Z41_Z4");
+    cut_Z21_Z41 = (TCutG*)infile->Get("cut_Z21_Z41");
     cut_x2_AoQs2s4 = (TCutG*)infile->Get("cut_x2_AoQs2s4");
     cut_x4_AoQs2s4 = (TCutG*)infile->Get("cut_x4_AoQs2s4");
+    cut_x2_AoQs1s2 = (TCutG*)infile->Get("cut_x2_AoQs1s2");
     cut_dEdegZ41_Z41 = (TCutG*)infile->Get("cut_dEdeg_Z41");
-    cut_sci42E_Z41 = (TCutG*)infile->Get("cut_sci42E_Z41");
 
-    c4LOG(info, Form("PID gate %s loaded with cuts on Z41/AoQs2s4 = %i, Z41/Z42 = %i, x2/AoQs2s4 = %i, x4/AoQs2s4 = %i, dEdegZ41/Z41 = %i, sci42E/Z41 = %i",
+    // c4LOG(info, Form("PID gate %s loaded with cuts on Z41/AoQs2s4 = %i, Z41/Z42 = %i, x2/AoQs2s4 = %i, x4/AoQs2s4 = %i, dEdegZ41/Z41 = %i, sci42E/Z41 = %i",
+    // gate_name.c_str(),
+    // cut_Z41_AoQs2s4!=nullptr,
+    // cut_Z41_Z42!=nullptr,
+    // cut_x2_AoQs2s4!=nullptr,
+    // cut_x4_AoQs2s4!=nullptr,
+    // cut_dEdegZ41_Z41!=nullptr,
+    // cut_sci42E_Z41!=nullptr));
+
+    c4LOG(info, Form("PID gate %s loaded with cuts on Z21/AoQs1s2 = %i, Z41/AoQs2s4 = %i, Z21/Z41 = %i, x2/AoQs2s4 = %i, x4/AoQs2s4 = %i, x2/AoQs1s2 = %i, dEdegZ41/Z41 = %i ",
     gate_name.c_str(),
+    cut_Z21_AoQs1s2!=nullptr,
     cut_Z41_AoQs2s4!=nullptr,
-    cut_Z41_Z42!=nullptr,
+    cut_Z21_Z41!=nullptr,
     cut_x2_AoQs2s4!=nullptr,
     cut_x4_AoQs2s4!=nullptr,
-    cut_dEdegZ41_Z41!=nullptr,
-    cut_sci42E_Z41!=nullptr));
+    cut_x2_AoQs1s2!=nullptr,
+    cut_dEdegZ41_Z41!=nullptr));
 
     //::: Drift corrected
     cut_Z41_AoQs2s4_driftcorr = (TCutG*)infile->Get("cut_Z41_AoQs2s4_driftcorr");
@@ -36,10 +53,6 @@ FrsGate::FrsGate(std::string gn, std::string file_name)
     }
 
     infile->Close();
-
-
-
-
 };
 
 FrsGate::~FrsGate()
@@ -54,26 +67,38 @@ std::string FrsGate::GetName()
 };
 
 
-bool FrsGate::PassedGate(double Z41, double Z42, double x2, double x4, double AoQs2s4, double dEdegZ41, double sci42E)
+bool FrsGate::PassedGate(double Z21, double Z41, double x2, double x4, double AoQs2s4,double AoQs1s2, double dEdegZ41)
 {
+    // bool passed_gate_cut_Z41_AoQs2s4 = false;
+    // bool passed_gate_cut_Z41_Z42 = false;
+    // bool passed_gate_cut_x2_AoQs2s4 = false;
+    // bool passed_gate_cut_x4_AoQs2s4 = false;
+    // bool passed_gate_cut_dEdegZ41_Z41 = false;
+    // bool passed_gate_cut_sci42E_Z41 = false;
+
+    bool passed_gate_cut_Z21_AoQs1s2 = false;
     bool passed_gate_cut_Z41_AoQs2s4 = false;
-    bool passed_gate_cut_Z41_Z42 = false;
+    bool passed_gate_cut_Z21_Z41 = false;
     bool passed_gate_cut_x2_AoQs2s4 = false;
     bool passed_gate_cut_x4_AoQs2s4 = false;
+    bool passed_gate_cut_x2_AoQs1s2 = false;
     bool passed_gate_cut_dEdegZ41_Z41 = false;
-    bool passed_gate_cut_sci42E_Z41 = false;
 
     //NB ORDER OF GATES!!
-    
+    if (cut_Z21_AoQs1s2 != nullptr) {
+        passed_gate_cut_Z21_AoQs1s2 = cut_Z21_AoQs1s2->IsInside(AoQs1s2,Z21);
+    }else{
+        passed_gate_cut_Z21_AoQs1s2 = true;
+    }
     if (cut_Z41_AoQs2s4 != nullptr) {
         passed_gate_cut_Z41_AoQs2s4 = cut_Z41_AoQs2s4->IsInside(AoQs2s4,Z41);
     }else{
         passed_gate_cut_Z41_AoQs2s4 = true;
     }
-    if (cut_Z41_Z42 != nullptr) {
-        passed_gate_cut_Z41_Z42 = cut_Z41_Z42->IsInside(Z42,Z41);
+    if (cut_Z21_Z41 != nullptr) {
+        passed_gate_cut_Z21_Z41 = cut_Z21_Z41->IsInside(Z41,Z21);
     }else{
-        passed_gate_cut_Z41_Z42 = true;
+        passed_gate_cut_Z21_Z41 = true;
     }
     if (cut_x2_AoQs2s4 != nullptr) {
         passed_gate_cut_x2_AoQs2s4 = cut_x2_AoQs2s4->IsInside(AoQs2s4,x2);
@@ -85,19 +110,28 @@ bool FrsGate::PassedGate(double Z41, double Z42, double x2, double x4, double Ao
     }else{
         passed_gate_cut_x4_AoQs2s4 = true;
     }
+    if (cut_x2_AoQs1s2 != nullptr) {
+        passed_gate_cut_x2_AoQs1s2 = cut_x2_AoQs1s2->IsInside(AoQs1s2,x2);
+    }else{
+        passed_gate_cut_x2_AoQs1s2 = true;
+    }
     if (cut_dEdegZ41_Z41 != nullptr) {
         passed_gate_cut_dEdegZ41_Z41 = cut_dEdegZ41_Z41->IsInside(dEdegZ41,Z41);
     }else{
         passed_gate_cut_dEdegZ41_Z41 = true;
     }
-    if (cut_sci42E_Z41 != nullptr) {
-        passed_gate_cut_sci42E_Z41 = cut_sci42E_Z41->IsInside(sci42E,Z41);
-    }else{
-        passed_gate_cut_sci42E_Z41 = true;
-    }
 
-    return passed_gate_cut_Z41_AoQs2s4 && passed_gate_cut_Z41_Z42 && passed_gate_cut_x2_AoQs2s4 && passed_gate_cut_x4_AoQs2s4 && passed_gate_cut_dEdegZ41_Z41 && passed_gate_cut_sci42E_Z41;
+    return passed_gate_cut_Z21_AoQs1s2 && passed_gate_cut_Z41_AoQs2s4 && passed_gate_cut_Z21_Z41 && passed_gate_cut_x2_AoQs2s4 && passed_gate_cut_x4_AoQs2s4 && passed_gate_cut_x2_AoQs1s2 && passed_gate_cut_dEdegZ41_Z41;
 };
+
+bool FrsGate::Passed_Z21vsAoQs1s2(double Z21, double AoQs1s2)
+{
+    bool passed_gate_cut_Z21_AoQs1s2 = false;
+    if (cut_Z21_AoQs1s2 != nullptr) passed_gate_cut_Z21_AoQs1s2 = cut_Z21_AoQs1s2->IsInside(AoQs1s2, Z21);
+    else passed_gate_cut_Z21_AoQs1s2 = true;
+    
+    return passed_gate_cut_Z21_AoQs1s2;
+}
 
 bool FrsGate::Passed_Z41vsAoQs2s4(double Z41, double AoQs2s4)
 {
@@ -108,13 +142,13 @@ bool FrsGate::Passed_Z41vsAoQs2s4(double Z41, double AoQs2s4)
     return passed_gate_cut_Z41_AoQs2s4;
 }
 
-bool FrsGate::Passed_Z41vsZ42(double Z41, double Z42)
+bool FrsGate::Passed_Z21vsZ41(double Z21, double Z41)
 {
-    bool passed_gate_cut_Z41_Z42 = false;
-    if (cut_Z41_Z42 != nullptr) passed_gate_cut_Z41_Z42 = cut_Z41_Z42->IsInside(Z42, Z41);
-    else passed_gate_cut_Z41_Z42 = true;
+    bool passed_gate_cut_Z21_Z41 = false;
+    if (cut_Z21_Z41 != nullptr) passed_gate_cut_Z21_Z41 = cut_Z21_Z41->IsInside(Z41, Z21);
+    else passed_gate_cut_Z21_Z41 = true;
     
-    return passed_gate_cut_Z41_Z42;
+    return passed_gate_cut_Z21_Z41;
 }
 
 bool FrsGate::Passed_x2vsAoQs2s4(double x2, double AoQs2s4)
@@ -135,6 +169,15 @@ bool FrsGate::Passed_x4vsAoQs2s4(double x4, double AoQs2s4)
     return passed_gate_cut_x4_AoQs2s4;
 }
 
+bool FrsGate::Passed_x2vsAoQs1s2(double x2, double AoQs1s2)
+{
+    bool passed_gate_cut_x2_AoQs1s2 = false;
+    if (cut_x2_AoQs1s2 != nullptr) passed_gate_cut_x2_AoQs1s2 = cut_x2_AoQs1s2->IsInside(AoQs1s2, x2);
+    else passed_gate_cut_x2_AoQs1s2 = true;
+    
+    return passed_gate_cut_x2_AoQs1s2;
+}
+
 bool FrsGate::Passed_dEdegZ41vsZ41(double Z41, double dEdegZ41)
 {
     bool passed_gate_cut_dEdegZ41_Z41 = false;
@@ -142,15 +185,6 @@ bool FrsGate::Passed_dEdegZ41vsZ41(double Z41, double dEdegZ41)
     else passed_gate_cut_dEdegZ41_Z41 = true;
     
     return passed_gate_cut_dEdegZ41_Z41;
-}
-
-bool FrsGate::Passed_sci42EvsZ41(double Z41, double sci42E)
-{
-    bool passed_gate_cut_sci42E_Z41 = false;
-    if (cut_sci42E_Z41 != nullptr) passed_gate_cut_sci42E_Z41 = cut_sci42E_Z41->IsInside(sci42E, Z41);
-    else passed_gate_cut_sci42E_Z41 = true;
-    
-    return passed_gate_cut_sci42E_Z41;
 }
 
 bool FrsGate::Passed_Z41vsAoQs2s4_driftcorr(double Z41_driftcorr, double AoQs2s4_driftcorr)
