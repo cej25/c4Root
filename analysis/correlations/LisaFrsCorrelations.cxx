c544dfd5285adfb402029f444d5fc8c51498eda6
cej25 <c.jones@gsi.de>
2025-05-08 19:45:06 +0200

    ngfjkwe f?

--- analysis/correlations/LisaFrsCorrelations.cxx
+++ analysis/correlations/LisaFrsCorrelations.cxx
@@ -1142,7 +1142,7 @@ void LisaFrsCorrelations::Exec(Option_t* option)
     // ::: MWD 
     //int pair_count_MWD = std::min(mwd_gates.size(), FrsGates.size());
     int mh_counter_passed_s1s2_seq_mwd[pair_count_MWD] = {0};
-    int mh_counter_passed_s2s4_seq_mwd[pair_count_MWD] = {0};
+    int mh_counter_passed_s2s4_seq_mwd[pair_count_MWD][layer_number] = {0};
 
     for (int pair = 0; pair < pair_count_MWD; pair++)  
     {
@@ -1176,7 +1176,7 @@ void LisaFrsCorrelations::Exec(Option_t* option)
             // Loop for sequential gate S1S2S4
             for (int i = 0; i < AoQ_s2s4_mhtdc.size(); i++)
             {
-                if (mh_counter_passed_s2s4_seq_mwd[pair] > 0) break;
+                if (mh_counter_passed_s2s4_seq_mwd[pair][l] > 0) break;
                 nmultihit++;
                 if (FrsGates[pair]->PassedS2S4(z41_mhtdc.at(i), z42_mhtdc.at(i), x2_position, x4_position, AoQ_s2s4_mhtdc.at(i), dEdeg_z41_mhtdc.at(i), sci42e))
                 {
@@ -1197,7 +1197,7 @@ void LisaFrsCorrelations::Exec(Option_t* option)
                             h1_LISA_energy_MWD_xy_s1s2s4_gated[pair][l]->Fill(energy_MWD_xy_gated[pair][l][lisa_config->xpos_gate][lisa_config->ypos_gate].at(k));
                         }
                     }
-                    mh_counter_passed_s2s4_seq_mwd[pair]++;
+                    mh_counter_passed_s2s4_seq_mwd[pair][l]++;
                     tot_pass_s2s4++;
                 }                   
             }
