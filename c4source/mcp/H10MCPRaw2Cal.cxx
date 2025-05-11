c2d241ceaaf4cb27945d6ed84acbc19f3b9a4327
cej25 <c.jones@gsi.de>
2025-05-11 17:57:33 +0200

    debugging

--- c4source/mcp/H10MCPRaw2Cal.cxx
+++ c4source/mcp/H10MCPRaw2Cal.cxx
@@ -143,7 +143,7 @@ void H10MCPRaw2Cal::Exec(Option_t* option)
             {
                 //break condition - cant recover.
                 ihit = hits_in_fast_channel + hits_in_slow_channel + ihit - 1; // -1 cus it adds one when restarting the for-loop
-                std::cout << "hits in fast does not equal hits in slow channel" << std::endl; 
+                std::cout << "hits in fast does not equal hits in slow channel:: " << first_hit_in_fast_channel->Get_ch_ID() << std::endl; 
                 continue;
             }
 
