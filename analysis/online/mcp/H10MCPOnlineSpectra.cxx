2aaa538126e7475fea09d22be93cf3a619a8c2f1
cej25 <c.jones@gsi.de>
2025-05-12 11:35:14 +0200

    local changes

--- analysis/online/mcp/H10MCPOnlineSpectra.cxx
+++ analysis/online/mcp/H10MCPOnlineSpectra.cxx
@@ -65,23 +65,27 @@ InitStatus H10MCPOnlineSpectra::Init()
     dir_mcp = new TDirectory("MCPs", "MCPs", "", 0);
     histograms->Add(dir_mcp);
 
- 
-    	h1_dT = MakeTH1(dir_mcp, "F", "h1_dT", "Time Difference T2-T1", 10000, -100, 100);  
-    	h2_dX1_dT = MakeTH2(dir_mcp, "F", "h2_dX1_dT", "X12-X11 position vs Time Difference T2-T1", 100, -250, 250, 100, -100, 100); 
+ 	
+    	h1_dT = MakeTH1(dir_mcp, "F", "h1_dT", "Time Difference T2-T1", 10000, -100, 100,"dt [ns]", kPink, kBlack);  
+    	h2_dX1_dT = MakeTH2(dir_mcp, "F", "h2_dX1_dT", "X12-X11 position vs Time Difference T2-T1", 100, -250, 250, 100, -100, 100);
     	h2_dY1_dT = MakeTH2(dir_mcp, "F", "h2_dY1_dT", "Y12-Y11 position vs Time Difference T2-T1", 100, -250, 250, 100, -100, 100); 
     	h2_dX2_dT = MakeTH2(dir_mcp, "F", "h2_dX2_dT", "X22-X21 position vs Time Difference T2-T1", 100, -250, 250, 100, -100, 100); 
     	h2_dY2_dT = MakeTH2(dir_mcp, "F", "h2_dY2_dT", "Y22-Y21 position vs Time Difference T2-T1", 100, -250, 250, 100, -100, 100); 
     	
-    	h1_T1_SC42L = MakeTH1(dir_mcp, "F", "h1_T1_SC42", "Time Difference T1-SC42L", 1000, -1000, 1000); 
     	
-	MCP1Heatmap1 = MakeTH2(dir_mcp,"b", "MCP1Heatmap1", "MCP1 Heatmap 1" , 100, -250, 250, 100, -250, 250);
-	MCP2Heatmap1 = MakeTH2(dir_mcp,"b", "MCP2Heatmap1", "MCP2 Heatmap 1" , 100, -250, 250, 100, -250, 250);
+	MCP1Heatmap1 = MakeTH2(dir_mcp,"b", "MCP1Heatmap1", "MCP1 Heatmap 1" , 100, -250, 250, 100, -250, 250); 
+	MCP2Heatmap1 = MakeTH2(dir_mcp,"b", "MCP2Heatmap1", "MCP2 Heatmap 1" , 100, -250, 250, 100, -250, 250); 
 
-    h1_sc41_mcp1 = MakeTH1(dir_mcp, "I", "h1_sc41_mcp1", "SC41-MCP1 Time Diff", 1000, -1000, 1000);
-    h1_sc41_mcp2 = MakeTH1(dir_mcp, "I", "h1_sc41_mcp2", "SC41-MCP2 Time Diff", 1000, -1000, 1000);
-    h1_sc42_mcp1 = MakeTH1(dir_mcp, "I", "h1_sc42_mcp1", "SC42-MCP1 Time Diff", 1000, -1000, 1000);
-    h1_sc42_mcp2 = MakeTH1(dir_mcp, "I", "h1_sc42_mcp2", "SC42-MCP2 Time Diff", 1000, -1000, 1000);
-
+    h1_sc41_mcp1 = MakeTH1(dir_mcp, "I", "h1_sc41_mcp1", "SC41-MCP1 Time", 4000, 0, 1000,"dt [ns]", kSpring, kBlack);
+    h1_sc41_mcp2 = MakeTH1(dir_mcp, "I", "h1_sc41_mcp2", "SC41-MCP2 Time", 4000, 0, 1000,"dt [ns]", kSpring, kBlack);
+    h1_sc42_mcp1 = MakeTH1(dir_mcp, "I", "h1_sc42_mcp1", "SC42-MCP1 Time", 4000, 0, 1000,"dt [ns]", kSpring, kBlack);
+    h1_sc42_mcp2 = MakeTH1(dir_mcp, "I", "h1_sc42_mcp2", "SC42-MCP2 Time", 4000, 0, 1000,"dt [ns]", kSpring, kBlack);
+    h1_sc41_sc42 = MakeTH1(dir_mcp, "I", "h1_sc41_sc42", "SC41-SC42 Time", 1000, -10, 10,"dt [ns]", kCyan, kBlack);
+    h1_stefan_mcp1 = MakeTH1(dir_mcp, "I", "h1_stefan_mcp1", "STEFAN-MCP1 Time", 4000,0, 1000,"dt [ns]", kOrange, kBlack);
+    h1_stefan_mcp2 = MakeTH1(dir_mcp, "I", "h1_stefan_mcp2", "STEFAN-MCP2 Time", 4000, 0, 1000,"dt [ns]", kOrange, kBlack);
+    h1_stefan_sc41 = MakeTH1(dir_mcp, "I", "h1_stefan_sc41", "STEFAN-SC41 Time", 4000,0, 1000,"dt [ns]", kOrange, kBlack);
+    h1_stefan_sc42 = MakeTH1(dir_mcp, "I", "h1_stefan_sc42", "STEFAN-SC42 Time", 4000, 0, 1000,"dt [ns]", kOrange, kBlack);
+	//gStyle->SetPalette(kBlueRedYellow);
     run->GetHttpServer()->RegisterCommand("Reset_MCP_Histos", Form("/Objects/%s/->Reset_Histo()", GetName()));
 
 
@@ -133,8 +137,10 @@ void H10MCPOnlineSpectra::Exec(Option_t* option)
 	  
     if (fHitsMCP && fHitsMCP->GetEntriesFast() > 0)
     {   
+    
+    for(int ihit=0; ihit < fHitsMCP->GetEntriesFast(); ++ihit) {
         Long64_t mpc_wr = 0;
-        H10MCPTwinpeaksAnaData* hit = (H10MCPTwinpeaksAnaData*)fHitsMCP->At(0);
+        H10MCPTwinpeaksAnaData* hit = (H10MCPTwinpeaksAnaData*)fHitsMCP->At(ihit);
         if (!hit) return;
         mcp_wr = hit->wr_t;
 
@@ -154,25 +160,33 @@ void H10MCPOnlineSpectra::Exec(Option_t* option)
         SC42 = hit->SC42;
         DSSDAccept = hit->DSSDAccept;
 
-        if (SC41 > 0)
-        {
-            h1_sc41_mcp1->Fill(SC41-T1);
-            h1_sc41_mcp2->Fill(SC41-T2); 
-        }
-        if (SC42 > 0)
-        {
-            h1_sc42_mcp1->Fill(SC42-T1);
-            h1_sc42_mcp2->Fill(SC42-T2);  
-        }  
-	
+        //if (SC41 > 0)
+       // {
+        if(SC41!=0 && T1!=0) h1_sc41_mcp1->Fill(SC41-T1);
+        if(SC41!=0 && T2!=0) h1_sc41_mcp2->Fill(SC41-T2); 
+        //}
+        //if (SC42 > 0)
+       // {
+        if(SC42!=0 && T1!=0) h1_sc42_mcp1->Fill(SC42-T1);
+        if(SC42!=0 && T2!=0) h1_sc42_mcp2->Fill(SC42-T2);  
+       // }
+        if(SC42!=0 && SC41!=0) h1_sc41_sc42->Fill(SC41-SC42);
         h1_dT->Fill(T2 - T1);
-        h2_dX1_dT->Fill(X12-X11, T2-T1);
-        h2_dY1_dT->Fill(Y12-Y11, T2-T1);
-        h2_dX1_dT->Fill(X22-X21, T2-T1);
-        h2_dY1_dT->Fill(Y22-Y21, T2-T1);
         
-        MCP1Heatmap1->Fill(X12-X11, Y12-Y11);
-        MCP2Heatmap1->Fill(X22-X21, Y22-Y21);
+        if(X12!=0 && X11!=0) h2_dX1_dT->Fill(X12-X11, T2-T1);
+        if(Y12!=0 && Y11!=0) h2_dY1_dT->Fill(Y12-Y11, T2-T1);
+        if(X22!=0 && X21!=0) h2_dX2_dT->Fill(X22-X21, T2-T1);
+        if(Y22!=0 && Y21!=0) h2_dY2_dT->Fill(Y22-Y21, T2-T1);
+        
+        if(T1!=0 && DSSDAccept!=0) h1_stefan_mcp1->Fill(DSSDAccept-T1);
+        if(T2!=0 && DSSDAccept!=0) h1_stefan_mcp2->Fill(DSSDAccept-T2);
+        
+        if(SC41!=0 && DSSDAccept!=0) h1_stefan_sc41->Fill(DSSDAccept-T1);
+        if(SC42!=0 && DSSDAccept!=0) h1_stefan_sc42->Fill(DSSDAccept-T2);
+        
+        if(X12!=0 && X11!=0 && Y12!=0 && Y11!=0) MCP1Heatmap1->Fill(X12-X11, Y12-Y11);
+        if(X22!=0 && X21!=0 && Y22!=0 && Y21!=0) MCP2Heatmap1->Fill(X22-X21, Y22-Y21);
+        }
     }
 
     fNEvents++;
