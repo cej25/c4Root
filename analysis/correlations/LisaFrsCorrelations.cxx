9ebfefb3672dc1090adc5c26c501a72b0bfbf435
eligand <e.m.gandolfo@gsi.de>
2025-05-12 22:34:20 +0200

    choosing only 1 hit of the multihit

--- analysis/correlations/LisaFrsCorrelations.cxx
+++ analysis/correlations/LisaFrsCorrelations.cxx
@@ -654,14 +654,12 @@ InitStatus LisaFrsCorrelations::Init()
     {
         h2_Z21_vs_AoQs1s2_LISA_MWD_gated.resize(mwd_gate_number);
         h2_Z41_vs_AoQs2s4_LISA_MWD_gated.resize(mwd_gate_number);
-        h2_Z42_vs_AoQs2s4_LISA_MWD_gated.resize(mwd_gate_number);
         for (int gate_lisa = 0; gate_lisa < mwd_gates.size(); gate_lisa++)
         {       
             dir_LISA_mwd_gates[gate_lisa] = dir_gate_LISA_mwd->mkdir(TString(mwd_gates.at(gate_lisa)->GetName()));
             
             h2_Z21_vs_AoQs1s2_LISA_MWD_gated[gate_lisa].resize(layer_number);
             h2_Z41_vs_AoQs2s4_LISA_MWD_gated[gate_lisa].resize(layer_number);
-            h2_Z42_vs_AoQs2s4_LISA_MWD_gated[gate_lisa].resize(layer_number);
             dir_LISA_mwd_gates[gate_lisa]->cd();
             for ( int i = 0; i < layer_number; i++)
             {
@@ -679,15 +677,7 @@ InitStatus LisaFrsCorrelations::Init()
                                                         1000, frs_config->fMin_Z, frs_config->fMax_Z);
                 h2_Z41_vs_AoQs2s4_LISA_MWD_gated[gate_lisa][i]->GetXaxis()->SetTitle("AoQ - S2S4");
                 h2_Z41_vs_AoQs2s4_LISA_MWD_gated[gate_lisa][i]->GetYaxis()->SetTitle("Z(41)");
-                h2_Z41_vs_AoQs2s4_LISA_MWD_gated[gate_lisa][i]->SetOption("COLZ");
-
-                h2_Z42_vs_AoQs2s4_LISA_MWD_gated[gate_lisa][i] = new TH2F(Form("h2_Z42_vs_AoQs2s4_LISA_MWD_%i_gate_%i", i+1, gate_lisa), 
-                                                        Form("Z42 vs AoQ S4 - LISA_MWD %i - Gate %i",i+1, gate_lisa),  
-                                                        1500, frs_config->fMin_AoQ, frs_config->fMax_AoQ, 
-                                                        1000, frs_config->fMin_Z, frs_config->fMax_Z);
-                h2_Z42_vs_AoQs2s4_LISA_MWD_gated[gate_lisa][i]->GetXaxis()->SetTitle("AoQ - S2S4");
-                h2_Z42_vs_AoQs2s4_LISA_MWD_gated[gate_lisa][i]->GetYaxis()->SetTitle("Z(42)");
-                h2_Z42_vs_AoQs2s4_LISA_MWD_gated[gate_lisa][i]->SetOption("COLZ");  
+                h2_Z41_vs_AoQs2s4_LISA_MWD_gated[gate_lisa][i]->SetOption("COLZ");   
             } 
 
         }
@@ -1279,12 +1269,15 @@ void LisaFrsCorrelations::Exec(Option_t* option)
     // CEJ :: Process FRS Gate info here first.
     for (int gate = 0; gate < FrsGates.size(); gate++)
     {
+        if (AoQ_s1s2_mhtdc.size()!=1 || AoQ_s2s4_mhtdc.size() !=1 )break;
         for (int i = 0; i < AoQ_s1s2_mhtdc.size(); i++)
         {
+            
             if (FrsGates[gate]->PassedS1S2(z21_mhtdc.at(i), x2_position, AoQ_s1s2_mhtdc.at(i)))
             {
                 z21_passed[gate].emplace_back(z21_mhtdc.at(i));
                 AoQ_s1s2_passed[gate].emplace_back(AoQ_s1s2_mhtdc.at(i));
+                
             }
         }
 
@@ -1448,32 +1441,6 @@ void LisaFrsCorrelations::Exec(Option_t* option)
         } 
 
 
-        if (!FrsGates.empty())
-        {
-            if (gate == 0)
-            {    
-                // S1S2 gated
-                if ( z21_passed[gate].size() > 0 )
-                {
-                    c4LOG(info, "::: Passed in S1S2");
-                    c4LOG(info, " z21 passed: " << z21_passed[gate] << " AoQ s1s2 passed : " << AoQ_s1s2_passed[gate]);
-                }
-                // S2S4
-                if ( z41_passed[gate].size() > 0) 
-                {
-                    c4LOG(info, "::: Passed in S2S4");
-                    c4LOG(info, " z41 passed: " << z41_passed[gate] << " AoQ s2s4 passed : " << AoQ_s2s4_passed[gate]);
-                }
-                // Sequential
-                if ( z21_passed[gate].size() > 0 && z41_passed[gate].size() > 0) 
-                {
-                    c4LOG(info," ::: Passed in SEQUENTIAL");
-                    c4LOG(info, " z21 passed: " << z21_passed[gate] << " AoQ s1s2 passed : " << AoQ_s1s2_passed[gate] <<" z41 passed: " << z41_passed[gate] << " AoQ s2s4 passed : " << AoQ_s2s4_passed[gate]);
-
-                }
-                c4LOG(info," :::::::::::::::: ");
-            }            
-        }
         multi_evt++;
         //..........................
         
@@ -1629,8 +1596,7 @@ void LisaFrsCorrelations::Exec(Option_t* option)
             // S2S4
             for (int i = 0; i < AoQ_s2s4_mhtdc.size(); i++)
             {
-                // h2_Z41_vs_AoQs2s4_LISA_MWD_gated[g][l]->Fill(AoQ_s2s4_mhtdc.at(i),z41_mhtdc.at(i));
-                h2_Z42_vs_AoQs2s4_LISA_MWD_gated[g][l]->Fill(AoQ_s2s4_mhtdc.at(i),z42_mhtdc.at(i));
+                h2_Z41_vs_AoQs2s4_LISA_MWD_gated[g][l]->Fill(AoQ_s2s4_mhtdc.at(i),z41_mhtdc.at(i));
             }            
         }
     }
