3b98cea9657695356be115cbd3e51bf0a2a9624c
Dennis Bittner <dbittner@lxi105.gsi.de>
2025-08-18 13:49:21 +0200

    simple gates to mcp

--- analysis/nearline/mcp/H10MCPNearlineSpectra.h
+++ analysis/nearline/mcp/H10MCPNearlineSpectra.h
@@ -4,7 +4,7 @@
 #include "FairTask.h"
 #include "TDirectory.h"
 #include "TH10MCPConfiguration.h"
-#include "H10MCPTwinpeaksCalData.h"
+#include "H10MCPTwinpeaksAnaData.h"
 #include "TFolder.h"
 #include "TH1F.h"
 #include "TH2F.h"
@@ -141,78 +141,83 @@ class H10MCPNearlineSpectra : public FairTask
         std::vector<std::pair<double,double>> dt_reference_detectors_energy_gates = {};
         int number_reference_detectors = 0;
         
-        // Histograms 
-        TH1* h1_test_histogram;
-        TH1* h1_time;
-        TH2* h2_MCP1;
-        TH2* h1_MCP2;
-        TH2* histogram2;
+        
+        // Histograms new //
+        TH1* h1_dT;
+        TH1* h1_T1_SC42L;
+        TH2* h2_dX1_dT;
+        TH2* h2_dY1_dT;
+        TH2* h2_dX2_dT;
+        TH2* h2_dY2_dT;
+       
         //TH1* MCP1Energy;
         //TH1* MCP2Energy;
-        TH2* MCP1Heatmap;
-        TH2* MCP2Heatmap;
+        TH2* MCP1Heatmap1;
+        TH2* MCP2Heatmap1;
+       // TH2* h2_dt_vsSC41;
+
+        TH1* h1_sc41_mcp1;
+        TH1* h1_sc41_mcp2;
+        TH1* h1_sc42_mcp1;
+        TH1* h1_sc42_mcp2;
+        TH1* h1_stefan_mcp1;
+        TH1* h1_stefan_mcp2;
+        TH1* h1_sc41_sc42;
+        TH1* h1_stefan_sc41;
+        TH1* h1_stefan_sc42;
+        // new //
+
+
+        // old // 
         TH1* MCP1X1ddiff;
         TH1* MCP1Y1ddiff;
-        
-        
-        TH2* MCP2HeatmapgatedMCP1up;
-        TH2* MCP2HeatmapgatedMCP1middle;
-        TH2* MCP2HeatmapgatedMCP1bottom;
+     //   TH2* MCP2HeatmapgatedMCP1up;
+     //   TH2* MCP2HeatmapgatedMCP1middle;
+     //   TH2* MCP2HeatmapgatedMCP1bottom;
         TH1* MCPtimegatedwith2positiongate;
+        TH2* MCP2HeatmapgatedMCP1right;
+        TH2* MCP2HeatmapgatedMCP1center;
+        //notation What to see "MCP2Heatmap" Option "gatedMCP1" "postion= "O" at gsi leftright etc  up middle down is a shift to the gates middle is neutral  
+       // TH2* MCP2HeatmapgatedMCP10middle;
+       // TH2* MCP2HeatmapgatedMCP1Oup;
+       // TH2* MCP2HeatmapgatedMCP1Odown;
+       // TH2* MCP2HeatmapgatedMCP1centermiddle;
+      //  TH2* MCP2HeatmapgatedMCP1centerup;
+      //  TH2* MCP2HeatmapgatedMCP1centerdown;
+       
+       
+       
+        ULong64_t entry2 = 0;
+        ULong64_t lastProcessedEntry = 0;
+        ULong64_t t2search;
+	    int ttrigger = 0;
+	    int maxFileCount = 0;
+        int ch0counter = 0;
+        int ch1counter = 0;
+        int ch2counter = 0;
+        int ch3counter = 0;
+        int ch4counter = 0;
+        int ch5counter = 0;
+        int eventcounter = 0;
+        int dataPointsCounter = 0;
+        int fileCount = 0;
+        bool foundMatchingFile = false;
+        int reruns = 0;
         
-       //notation What to see "MCP2Heatmap" Option "gatedMCP1" "postion= "O" at gsi leftright etc  up middle down is a shift to the gates middle is neutral  
-        TH2* MCP2HeatmapgatedMCP1Omiddle;
-        TH2* MCP2HeatmapgatedMCP1Oup;
-          TH2* MCP2HeatmapgatedMCP1Odown;
 
-          TH2* MCP2HeatmapgatedMCP1centermiddle;
-        TH2* MCP2HeatmapgatedMCP1centerup;
-          TH2* MCP2HeatmapgatedMCP1centerdown;
-       
-       
-       
-       	  ULong64_t entry2 = 0;
-	  ULong64_t lastProcessedEntry = 0;
-	  ULong64_t t2search;
-	  int ttrigger = 0;
-	  int maxFileCount = 0;
-	   int ch0counter = 0;
-	  int ch1counter = 0;
-	  int ch2counter = 0;
-	  int ch3counter = 0;
-	  int ch4counter = 0;
-	  int ch5counter = 0;
-	  int eventcounter = 0;
-	  int dataPointsCounter = 0;
-	  int fileCount = 0;
-	  bool foundMatchingFile = false;
-	  int reruns = 0;
-	  	   // zwischenspeicher fuereventbuilding
-	  ULong64_t ttrigger0; // T1 hat leider der CAEN stempel geklaut
-	   ULong64_t T1;
-	  double T01=0;
-	  double T02=0;
-	  double E1=0;
-	  double X01=0;
-	  double X02=0;
-	  double Y01=0;
-	  double Y02=0;
-	  double X11=0;
-	  double X12=0;
-	  double Y11=0;
-	  double Y12=0;
-	  
-	  double MCP1Xdiff;
-	  double MCP1Ydiff;
-	  double MCP2Xdiff;
-	  double MCP2Ydiff;
+        double MCP1Xdiff;
+        double MCP1Ydiff;
+        double MCP2Xdiff;
+        double MCP2Ydiff;
+
 		// GATES
 		
 		
 		
 
 int gatesmcp1T[4]={35,45,47,57};
- int gatesmcp1center[4]={1,5,14,17};
+ int gatesmcp1center[4]={-10,-10,10,10};
+int gatesmcpright [4]={60,00,80,10};
     
     
 //int gatesmcp1T [] = {35,	45,	47,	57};
@@ -223,7 +228,8 @@ TBox *gateBoxT = new TBox(gatesmcp1T[0], gatesmcp1T[1],
  TBox *gateBoxcenter = new TBox(gatesmcp1center[0], gatesmcp1center[1],
                          gatesmcp1center[2], gatesmcp1center[3]);                        
                
-        
+     TBox *gateBoxright = new TBox(gatesmcpright[0], gatesmcpright[1],
+                         gatesmcpright[2], gatesmcpright[3]);          
 
 
         std::vector<TH1*> h1_fatima_slowToT;
