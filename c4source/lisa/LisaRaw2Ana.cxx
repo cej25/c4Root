8e1ed854804d316f9cb051fc7c940d7aeddb3bbe
cej25 <c.jones@gsi.de>
2025-10-14 15:37:39 +0200

    atimaaa

--- c4source/lisa/LisaRaw2Ana.cxx
+++ c4source/lisa/LisaRaw2Ana.cxx
@@ -116,7 +116,7 @@ void LisaRaw2Ana::Exec(Option_t* option)
     // at.DefineTarget(water);
     // at.DefineProjectile(carbon);
 
-    at.Calculate(carbon, water, 400);//hello
+    at.Calculate(carbon, water, 400);
     
     
 
